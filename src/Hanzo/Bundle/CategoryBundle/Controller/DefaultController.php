ca2e29380c7bde96580cd019ba960d68f437831e
ulrik nielsen <ulrik@o3.dk>
2011-12-15 10:42:16 +0100

    code cleanup

--- src/Hanzo/Bundle/CategoryBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/CategoryBundle/Controller/DefaultController.php
@@ -2,14 +2,101 @@
 
 namespace Hanzo\Bundle\CategoryBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Symfony\Component\DependencyInjection\ContainerAware,
+    Symfony\Component\HttpFoundation\Response
+;
 
+use Hanzo\Core\CoreController;
 
-class DefaultController extends Controller
+use Hanzo\Model\ProductsImagesCategoriesSortQuery,
+    Hanzo\Model\ProductsImagesCategoriesSortPeer
+;
+
+class DefaultController extends CoreController
 {
-    
-    public function indexAction($name)
+
+    /**
+     * handle category listings
+     *
+     * @param $cms_id
+     * @param $category_id
+     * @param $pager
+     */
+    public function viewAction($cms_id, $category_id, $pager = 1)
     {
-        return $this->render('CategoryBundle:Default:index.html.twig', array('name' => $name));
+        $route = $this->get('request')->get('_route');
+
+        $cache_id = explode('_', $route);
+        $cache_id = array($cache_id[0], $cache_id[2], $cache_id[1]);
+
+        $data = $this->getCache($cache_id);
+        if (!$data) {
+
+            $locale = $this->get('hanzo')->get('core.locale');
+            $router = $this->get('router');
+
+            $result = ProductsImagesCategoriesSortQuery::create()
+                ->useProductsQuery()
+                    ->filterByIsOutOfStock(FALSE)
+                ->endUse()
+                ->joinWithProducts()
+                ->joinWithProductsImages()
+                ->orderBySort()
+                ->filterByCategoriesId($category_id)
+                ->paginate($pager, 12)
+            ;
+
+            $product_route = str_replace('category_', 'product_', $route);
+
+            $records = array();
+            foreach ($result as $record) {
+                $product = $record->getProducts();
+                $records[] = array(
+                    'sku' => $product->getSku(),
+                    'id' => $product->getId(),
+                    'name' => $product->getMaster(),
+                    'image' => 'http://static.hanzo.bc/images/products/' . $record->getProductsImages()->getImage(),
+                    'url' => $router->generate($product_route, array(
+                        'product_id' => $product->getId(),
+                        'title' => $this->stripText($product->getMaster()),
+                    )),
+                    // TODO prices ?
+                );
+            }
+
+            $data = array(
+                'title' => 'cat view',
+                'products' => $records,
+            );
+
+            if ($result->haveToPaginate()) {
+
+                $pages = array();
+                foreach ($result->getLinks(20) as $page) {
+                    $pages[$page] = $router->generate($route, array('pager' => $page), TRUE);
+                }
+
+                $data['paginate'] = array(
+                    'next' => ($result->getNextPage() == $pager ? '' : $router->generate($route, array('pager' => $result->getNextPage()), TRUE)),
+                    'prew' => ($result->getPreviousPage() == $pager ? '' : $router->generate($route, array('pager' => $result->getPreviousPage()), TRUE)),
+
+                    'pages' => $pages,
+                    'index' => $pager
+                );
+            }
+
+            // categories are cached 5 seconds - just to boost performance a bit.
+            $this->setCache($cache_id, $data, 5);
+        }
+
+        if ($this->getFormat() == 'json') {
+            $this->json_responce($data);
+        }
+
+        $this->get('twig')->addGlobal('page_type', 'category');
+        $responce = $this->render('HanzoCategoryBundle:Default:view.html.twig', $data);
+
+        return $responce;
     }
 }
--- src/Hanzo/Bundle/CategoryBundle/DependencyInjection/Configuration.php
+++ src/Hanzo/Bundle/CategoryBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('category');
+        $rootNode = $treeBuilder->root('hanzo_category');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
