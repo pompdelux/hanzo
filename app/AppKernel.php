f69e330e4f97e2ebee99d1008bcf531ae58bb20e
ulrik nielsen <ulrik@o3.dk>
2012-01-12 14:26:37 +0100

    not my bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,7 +30,6 @@ class AppKernel extends Kernel
             new Hanzo\Bundle\ProductBundle\HanzoProductBundle(),
             new Hanzo\Bundle\SearchBundle\HanzoSearchBundle(),
             new Hanzo\Bundle\BasketBundle\BasketBundle(),
-            new Hanzo\Bundle\PaymentBundle\PaymentBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
