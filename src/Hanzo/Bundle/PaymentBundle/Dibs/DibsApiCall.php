03a7113784eec2a6ba071a6998e53b64f98f8177
Henrik Farre <henrik@bellcom.dk>
2012-01-12 14:30:18 +0100

    Dibs

---
+++ src/Hanzo/Bundle/PaymentBundle/Dibs/DibsApiCall.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Hanzo\Bundle\PaymentBundle\Dibs;
+
+class DibsApiCall
+{
+  /**
+   * undocumented class variable
+   *
+   * @var DibsApiCall instance 
+   **/
+  private static $instance = null;
+
+  /**
+   * undocumented class variable
+   *
+   * @var string
+   **/
+  protected $baseUrl = 'https://payment.architrade.com/';
+
+  /**
+   * __construct
+   * @return void
+   * @author Henrik Farre <hf@bellcom.dk>
+   **/
+  private function __construct() {}
+
+  /**
+   * someFunc
+   * @return void
+   * @author Henrik Farre <hf@bellcom.dk>
+   **/
+    public static function getInstance($settings)
+    {
+      if ( self::$instance === null )
+      {
+        self::$instance = new self;
+      }
+
+      self::$instance->settings = $settings;
+
+      return self::$instance;
+    }
+
+  /**
+   * execute
+   * @return void
+   * @author Henrik Farre <hf@bellcom.dk>
+   **/
+  public function execute( $url, array $params, $useAuthHeaders = false )
+  {
+    $logger = Hanzo::getInstance()->container->get('logger');
+
+    if ( isset( $params['merchant'] ) && empty( $params['merchant'] ) )
+    {
+      throw new Exception('Missing merchant number');
+    }
+
+    $ch = curl_init();
+
+    $url = $this->baseUrl . $url;
+
+    curl_setopt($ch, CURLOPT_URL, $url );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+
+    $headers = array();
+    if ( $useAuthHeaders )
+    {
+      if ( !isset($this->settings['api_user']) || !isset($this->settings['api_pass']) )
+      {
+        throw new Exception( 'DIBS api: Missing api username or/and password' );
+      }
+
+      $headers = array( 'Authorization: Basic '. base64_encode($this->settings['api_user'].':'.$this->settings['api_pass']) );
+
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers );
+    }
+
+    //if ( $this->debug )
+    //{
+      //$msg = 'Calling Dibs api on url: '.$url.PHP_EOL.'Params:'.print_r($headers,1).PHP_EOL.print_r($params,1);
+      //$logger->debug($msg)
+    //}
+    //$this->debugMsg( 'Action: "'. $this->currentAction .'" data: '. print_r($params,1) , __FUNCTION__, __LINE__ );
+
+    $response = curl_exec($ch);
+
+    if ( $response === false )
+    {
+      curl_close($ch);
+      throw new Exception( 'Kommunikation med DIBS fejlede, fejlen var: "'.curl_error($ch).'"' );
+      error_log(__LINE__.':'.__FILE__.' Curl error: ' . curl_error($ch));
+    }
+
+    curl_close($ch);
+
+    return $response;
+  }
+}
---
+++ src/Hanzo/Bundle/PaymentBundle/Dibs/DibsApiCallException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Hanzo\Bundle\PaymentBundle\Dibs;
+
+class DibsApiCallException
+{
+}
