05beb6ab8e77ae27e575b741764b87cd391476bf
Henrik Farre <henrik@bellcom.dk>
2012-01-18 14:17:26 +0100

    Newsletter js and template

--- src/Hanzo/Bundle/NewsletterBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/NewsletterBundle/Controller/DefaultController.php
@@ -1,15 +1,29 @@
-<?php
+<?php /* vim: set sw=4: */
 
 namespace Hanzo\Bundle\NewsletterBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Symfony\Component\HttpFoundation\Response,
+    Symfony\Component\HttpFoundation\Request;
 
+use Hanzo\Core\Hanzo,
+    Hanzo\Core\Tools,
+    Hanzo\Core\CoreController;
 
-class DefaultController extends Controller
+class DefaultController extends CoreController
 {
-    
     public function indexAction($name)
     {
         return $this->render('NewsletterBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * testAction
+     * @return void
+     * @author Henrik Farre <hf@bellcom.dk>
+     **/
+    public function testAction()
+    {
+        return $this->render('NewsletterBundle:Default:test.html.twig',array('page_type' => 'newsletter'));
     }
 }
