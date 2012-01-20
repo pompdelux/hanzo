d1d36e88c67de6d63d9fcbbab304dcd9b5bca266
Henrik Farre <henrik@bellcom.dk>
2012-01-20 14:09:57 +0100

    routing and bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new Hanzo\Bundle\BasketBundle\BasketBundle(),
             new Hanzo\Bundle\PaymentBundle\PaymentBundle(),
             new Hanzo\Bundle\NewsletterBundle\NewsletterBundle(),
+            new Hanzo\Bundle\CheckoutBundle\CheckoutBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
