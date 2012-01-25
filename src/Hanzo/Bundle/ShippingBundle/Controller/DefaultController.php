7615becf2ec39b82f57cd1e774e5383d56316f86
Henrik Farre <henrik@bellcom.dk>
2012-01-25 21:33:42 +0100

    Shipping

--- src/Hanzo/Bundle/ShippingBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/ShippingBundle/Controller/DefaultController.php
@@ -1,13 +1,25 @@
-<?php
+<?php /* vim: set sw=4: */
 
 namespace Hanzo\Bundle\ShippingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Hanzo\Core\Hanzo,
+    Hanzo\Core\CoreController
+    ;
 
 class DefaultController extends Controller
 {
-    
+    /**
+     * blockAction
+     * @return void
+     * @author Henrik Farre <hf@bellcom.dk>
+     **/
+    public function blockAction()
+    {
+        return $this->render('ShippingBundle:Default:block.html.twig');
+    }
+
     public function indexAction($name)
     {
         return $this->render('ShippingBundle:Default:index.html.twig', array('name' => $name));
