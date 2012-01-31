d9c226fb578d2206589a0c269d7e4271e88b9f64
Henrik Farre <henrik@bellcom.dk>
2012-01-31 10:46:30 +0100

    Checkout

--- src/Hanzo/Bundle/ShippingBundle/ShippingApi.php
+++ src/Hanzo/Bundle/ShippingBundle/ShippingApi.php
@@ -2,7 +2,8 @@
 
 namespace Hanzo\Bundle\ShippingBundle;
 
-use Hanzo\Bundle\ShippingBundle\ShippingMethods\ShippingMethod
+use Hanzo\Core\Hanzo,
+    Hanzo\Bundle\ShippingBundle\ShippingMethods\ShippingMethod
     ;
 
 /**
@@ -14,36 +15,63 @@ use Hanzo\Bundle\ShippingBundle\ShippingMethods\ShippingMethod
 class ShippingApi
 {
     /**
+     * undocumented class variable
+     *
+     * @var string
+     **/
+    protected $domainKey;
+
+    /**
      * __construct
      * @return void
      * @author Henrik Farre <hf@bellcom.dk>
      **/
     public function __construct()
     {
+        $this->domainKey = Hanzo::getInstance()->get('core.domain_key');;
     }
 
     /**
      * getMethods
-     * @return void
+     * @return array
      * @author Henrik Farre <hf@bellcom.dk>
      **/
     public function getMethods()
     {
-        $methods = array(
-            // DK:
-            '10' => new ShippingMethod( 'Post Danmark', 'Privat', 'Fragt til private', '10', 'flat' ),
-            '11' => new ShippingMethod( 'Post Danmark', 'Erhverv', 'Fragt til erhverv', '11', 'flat' ),
-            '12' => new ShippingMethod( 'Post Danmark', 'Døgnpost', '', '12', 'flat' ),
-            // COM:
-            '20' => new ShippingMethod( 'Post Danmark', 'Overseas', '', '20', 'flat' ),
-            // SE:
-            '30' => new ShippingMethod( 'Privat bulksplit', 'Privat bulksplit', '', '30', 'flat' ),
-            // NO:
-            'P' => new ShippingMethod( 'Post Danmark', 'På døren', '', 'P', 'flat' ),
-            'S' => new ShippingMethod( 'Post Danmark', 'Servicepakke', '', 'S', 'flat' ),
-            // NL:
-            '60' => new ShippingMethod( 'DPD', 'DPD', '', '60', 'flat' ),
-            );
+        $methods = array();
+
+        switch ($this->domainKey) 
+        {
+            case 'DK':
+                $methods = array(
+                    '10' => new ShippingMethod( 'Post Danmark', 'Privat', 'Fragt til private', '10', 'flat' ),
+                    '11' => new ShippingMethod( 'Post Danmark', 'Erhverv', 'Fragt til erhverv', '11', 'flat' ),
+                    '12' => new ShippingMethod( 'Post Danmark', 'Døgnpost', '', '12', 'flat' ),
+                );
+                break;
+            case 'COM':
+                $methods = array(
+                    '20' => new ShippingMethod( 'Post Danmark', 'Overseas', '', '20', 'flat' ),
+                );
+                break;
+            case 'SE':
+                $methods = array(
+                    '30' => new ShippingMethod( 'Privat bulksplit', 'Privat bulksplit', '', '30', 'flat' ),
+                );
+                break;
+            case 'NO':
+                $methods = array(
+                    'P' => new ShippingMethod( 'Post Danmark', 'På døren', '', 'P', 'flat' ),
+                    'S' => new ShippingMethod( 'Post Danmark', 'Servicepakke', '', 'S', 'flat' ),
+                );
+                break;
+            case 'NL':
+                $methods = array(
+                    '60' => new ShippingMethod( 'DPD', 'DPD', '', '60', 'flat' ),
+                );
+                break;
+        }
+
         return $methods;
     }
 } // END class ShippingApi
