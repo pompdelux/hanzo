f3d6caa067362478df926dfd9f4a966109d160a6
Henrik Farre <henrik@bellcom.dk>
2012-02-08 14:01:05 +0100

    Zip to city lookup

--- src/Hanzo/Bundle/ServiceBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/ServiceBundle/Controller/DefaultController.php
@@ -1,15 +1,51 @@
-<?php
+<?php /* vim: set sw=4: */
 
 namespace Hanzo\Bundle\ServiceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Hanzo\Core\CoreController,
+    Hanzo\Core\Hanzo,
+    Hanzo\Core\Tools
+    ;
 
-class DefaultController extends Controller
+use Hanzo\Model\ZipToCity,
+    Hanzo\Model\ZipToCityPeer,
+    Hanzo\Model\ZipToCityQuery
+    ;
+
+class DefaultController extends CoreController
 {
-    
+
     public function indexAction($name)
     {
         return $this->render('ServiceBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * getCityFromZipAction
+     * @return Response
+     * @author Henrik Farre <hf@bellcom.dk>
+     **/
+    public function getCityFromZipAction($zip_code)
+    {
+        $query = ZipToCityQuery::create()
+            ->filterByCountriesIso2('DK')
+            ->filterByZip( $zip_code )
+            ->findOne();
+
+        if ( !($query instanceOf ZipToCity) )
+        {
+            return $this->json_response(array( 
+                'status' => false,
+                'message' => '',
+            ));
+        }
+
+        return $this->json_response(array( 
+            'status' => true,
+            'message' => '',
+            'data' => array('city' => $query->getCity())
+        ));
     }
 }
--- web/fx/scripts/account.js
+++ web/fx/scripts/account.js
@@ -1,49 +1,90 @@
-(function(jQuery) {
-  var account = (function($) {
-    var pub = {}
+var account = (function($) {
+  var pub = {};
 
-    pub.init = function() {
-      $('form.nno').on('submit', function(event) {
-        event.preventDefault();
-        var $form = $(this);
-        var $input = $('input[name="phone"]', $form);
-        if ($input.val()) {
-          // trigger loading box
-          dialoug.loading('form.nno input[type="submit"]', 'søger efter adresse...');
-          // fetch data
-          $.getJSON(this.action + '/' + $input.val() , function(result) {
-            if (result.status) {
-              var $target = $('form.create');
-              $target.find('#customers_first_name').val(result.data.christianname);
-              $target.find('#customers_last_name').val(result.data.surname);
-              $target.find('#customers_addresses_0_address_line_1').val(result.data.address);
-              $target.find('#customers_addresses_0_postal_code').val(result.data.zipcode);
-              $target.find('#customers_addresses_0_city').val(result.data.district);
-              $target.find('#customers_phone').val(result.data.phone);
-            }
-            else {
-              dialoug.alert('Woops!', result.message);
-            }
-            // reset form and kill loader
-            $input.val('');
-            dialoug.stopLoading();
-          });
-        }
+  pub.init = function() {
 
-        return false;
-      });
+    zipToCityInit();
 
-      if($('form.create').length) {
-        var $form = $('form.create');
-        var $a = $('form.create a');
-        $form.find('label[for="customers_accept"]').append($a)
+    $('form.nno').on('submit', function(event) {
+      event.preventDefault();
+      var $form = $(this);
+      var $input = $('input[name="phone"]', $form);
+      if ($input.val()) {
+        // trigger loading box
+        dialoug.loading('form.nno input[type="submit"]', 'søger efter adresse...');
+        // fetch data
+        $.getJSON(this.action + '/' + $input.val() , function(result) {
+          if (result.status) {
+            var $target = $('form.create');
+            $target.find('#customers_first_name').val(result.data.christianname);
+            $target.find('#customers_last_name').val(result.data.surname);
+            $target.find('#customers_addresses_0_address_line_1').val(result.data.address);
+            $target.find('#customers_addresses_0_postal_code').val(result.data.zipcode);
+            $target.find('#customers_addresses_0_city').val(result.data.district);
+            $target.find('#customers_phone').val(result.data.phone);
+          }
+          else {
+            dialoug.alert('Woops!', result.message);
+          }
+          // reset form and kill loader
+          $input.val('');
+          dialoug.stopLoading();
+        });
       }
 
-      $('a[rel="colorbox"]').colorbox();
-    };
+      return false;
+    });
 
-    return pub;
-  })(jQuery);
+    if($('form.create').length) {
+      var $form = $('form.create');
+      var $a = $('form.create a');
+      $form.find('label[for="customers_accept"]').append($a);
+    }
 
-  account.init();
+    $('a[rel="colorbox"]').colorbox();
+  };
+
+  function zipToCityInit()
+  {
+    /**
+     * auto complete city names when entering zip codes.
+     * only works for se/no/dk so if the request is made via .com we skip this step
+     */
+    var tld = document.location.hostname.match(new RegExp("\.([a-z,A-Z]{2,6})$"));
+    try {
+      if (tld[1] != 'com' && tld[1] != 'nl') {
+        $('#customers_addresses_0_city').attr('readonly', 'readonly');
+
+        // set city to readonly
+        $('#customers_addresses_0_city').focus(function () {
+          this.value = '';
+          if ($('#customers_addresses_0_postal_code').val() == '') {
+            $('#customers_addresses_0_postal_code')
+            .css('border-color', '#a10000')
+            .fadeOut(100).fadeIn(100)
+            .fadeOut(100).fadeIn(100)
+            .fadeOut(100).fadeIn(100)
+            .focus();
+            return;
+          }
+          $.getJSON( base_url+'service/get-city-from-zip/'+$('#customers_addresses_0_postal_code').val(), function(data) {
+            if (data.status && data.data.city) {
+              $('#customers_addresses_0_city').val(data.data.city);
+              $('#customers_addresses_0_postal_code').css('border-color', '#444345');
+              try {
+                $('#customers_phone').focus();
+              } catch (e) {}
+            }
+            else {
+              $('#customers_addresses_0_postal_code').css('border-color', '#a10000').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).focus();
+            }
+          });
+        });
+      }
+    } catch (e) {}
+  }
+
+  return pub;
 })(jQuery);
+
+account.init();
