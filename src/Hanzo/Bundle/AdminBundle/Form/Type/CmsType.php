250dda00fcade3046dc1f2d48574435c1fd58e62
Anders Bryrup <andersbryrup@gmail.com>
2012-03-20 21:15:22 +0100

    Edit Cms Action

---
+++ src/Hanzo/Bundle/AdminBundle/Form/Type/CmsType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Hanzo\Bundle\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class CmsType extends AbstractType
+{
+    public function __construct($id = null)
+    {
+        $this->id = $id;
+    }
+
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+
+      $builder->add('is_active', 'checkbox', array(
+          'label'     => 'cms.edit.is_active',
+          'translation_domain' => 'admin',
+          'required'  => false
+      ));
+
+      $builder->add('title', null, array(
+          'required' => TRUE,
+          'translation_domain' => 'admin'
+      ));
+      $builder->add('path', null, array(
+          'required' => TRUE,
+          'translation_domain' => 'admin'
+      ));
+      $builder->add('content', 'textarea', array(
+          'required' => FALSE,
+          'translation_domain' => 'admin'
+      ));
+      $builder->add('settings', null, array(
+          'required' => FALSE,
+          'translation_domain' => 'admin'
+      ));
+
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+      return array(
+        'data_class' => 'Hanzo\Model\Cms',
+      );
+    }
+
+    public function getName()
+    {
+      return 'cms';
+    }
+}
