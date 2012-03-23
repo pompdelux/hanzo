b9b6f52cf10edc39cd930c4b18d8321d4e189b5a
ulrik nielsen <ulrik@o3.dk>
2012-03-23 14:35:01 +0100

    type-o fix

--- src/Hanzo/Core/Tools.php
+++ src/Hanzo/Core/Tools.php
@@ -203,7 +203,7 @@ class Tools
         if (is_null($gateway_id)) {
             $gateway_id = self::getNextSequenceId('payment gateway');
 
-            $env = Hanzo::getInstance()->get('kernel')->getEnvironment();
+            $env = Hanzo::getInstance()->container->get('kernel')->getEnvironment();
             if ($env != 'prod') {
                 $gateway_id = $env . '_' . $gateway_id;
             }
