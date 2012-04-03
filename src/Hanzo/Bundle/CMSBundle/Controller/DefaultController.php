9e655d1a4521905e7f4d1bdb097568492514dd1f
ulrik nielsen <ulrik@o3.dk>
2012-04-03 08:50:06 +0200

    added missing translations

--- src/Hanzo/Bundle/CMSBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/CMSBundle/Controller/DefaultController.php
@@ -26,57 +26,6 @@ class DefaultController extends CoreController
 
     public function viewAction($id, $page = NULL)
     {
-
-
-$mailer = $this->get('mail_manager');
-$order = \Hanzo\Model\OrdersQuery::create()->findPk(572830);
-//Tools::log(get_class_methods($order));
-
-$email = $order->getEmail();
-$name  = trim($order->getFirstName() . ' ' . $order->getLastName());
-$shippinng = $this->get('translator')->trans('shipping_method_name_' . $order->getDeliveryMethod(), array(), 'shipping');
-
-$params = array(
-    'order' => $order,
-    'payment_address' => Tools::orderAddress('payment', $order),
-    'company_address' => '',
-    'delivery_address' => Tools::orderAddress('shipping', $order),
-    'customer_id' => $order->getCustomersId(),
-    'order_date' => $order->getCreatedAt('Y-m-d'),
-    'payment_method' => $order->getBillingMethod(),
-    'shipping_title' => $shippinng,
-    'shipping_cost' => 0.00, // TODO
-    'payment_fee' => 0.00, // expected_at
-    'TODO' => '', // TODO
-    'username' => $order->getCustomers()->getEmail(),
-    'password' => $order->getCustomers()->getPasswordClear(),
-    'conditions' => '', // TODO
-    'expected_at' => '',
-    'card_type' => '',
-    'transaction_id' => '',
-);
-
-// TODO: only set if not null
-if(0){
-    $params['event_id'] = '';
-    $params['payment_gateway_id'] = '';
-    $params['coupon_amount'] = 0.00;
-    $params['coupon_name'] = '';
-    $params['hostess_discount'] = '';
-    $params['hostess_discount_title'] = '';
-    $params['gothia_fee'] = 0.00;
-    $params['gothia_fee_title'] = '';
-}
-
-$mailer->setMessage('order.confirmation', $params);
-$mailer->setTo($email, $name);
-// TODO: send confirmation mail to customer and bcc to store owner
-$mailer->send();
-
-
-
-
-
         $locale = Hanzo::getInstance()->get('core.locale');
 
         if ($page instanceof Cms) {
