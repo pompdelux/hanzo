1ee00d919a157e49cab4a0fef61f0b9a39be7030
Anders Bryrup <andersbryrup@gmail.com>
2012-04-11 11:51:05 +0200

    Able to add new settings. m.m.

--- src/Hanzo/Bundle/AdminBundle/Controller/SettingsController.php
+++ src/Hanzo/Bundle/AdminBundle/Controller/SettingsController.php
@@ -4,48 +4,29 @@ namespace Hanzo\Bundle\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Hanzo\Model\SettingsQuery;
+use Hanzo\Model\Settings,
+	Hanzo\Model\DomainsSettings,
+	Hanzo\Model\SettingsQuery,
+	Hanzo\Model\DomainsSettingsQuery,
+	Hanzo\Model\DomainsQuery;
 
 use Hanzo\Bundle\AdminBundle\Form\Type\SettingsType;
 
 class SettingsController extends Controller
 {
-    
+	/**
+	 * @todo Lav slet funktion til settings
+	 */    
     public function indexAction()
     {
-    	$general_settings = SettingsQuery::create()
-    		->orderByNs()
-    		->find()
-    	;
-
-    	$general_settings_list = array();
-    	foreach ($general_settings as $setting) {
-    		$general_settings_list[$setting->getCKey() . '__' . $setting->getNs()] = $setting->getCValue();
-    	}
-
-    	$form = $this->createFormBuilder($general_settings_list)
-	        ->add('api_user__dibsapi', 'text')
-	        ->add('api_pass__dibsapi', 'text')
-	        ->add('merchant__dibsapi', 'text')
-	        ->add('md5key1__dibsapi', 'text')
-	        ->add('md5key2__dibsapi', 'text')
-	        ->add('test__dibsapi', 'text')
-	        ->add('from_name__email', 'text')
-	        ->add('from_email__email', 'text')
-	        ->add('clientID__gothiaapi', 'text')
-	        ->add('password__gothiaapi', 'text')
-	        ->add('username__gothiaapi', 'text')
-	        ->getForm()
-	    ;
-
-		$request = $this->getRequest();
-
+    	$request = $this->getRequest();
         if ($request->getMethod() == 'POST') {
-            $form->bindRequest($request);
-
-            // data is an array with "api_user__dibsapi", "api_pass__dibsapi" keys
-            $data = $form->getData();
+            
+            $data = $request->get('form');
             foreach ($data as $key_ns => $c_value) {
+
+            	if('_token' == $key_ns) continue;
+
             	$keys = explode('__',$key_ns);
             	$c_key = $keys[0];
             	$ns = $keys[1];
@@ -63,13 +44,168 @@ class SettingsController extends Controller
             }
             $this->get('session')->setFlash('notice', 'settings.updated');
         }
-		return $this->render('AdminBundle:Settings:view.html.twig', array(
-			'form'      => $form->createView()
+
+		$form_add_global_setting = $this->createFormBuilder(new Settings())
+			->add('c_key', 'text')
+			->add('ns', 'text')
+			->add('title', 'text')
+			->add('c_value', 'text')
+			->getForm();
+
+	    // Generate the Form for the global settings
+
+		$global_settings = SettingsQuery::create()
+			->orderByNs()
+			->find()
+		;
+
+		$global_settings_list = array();
+		foreach ($global_settings as $setting) {
+			$global_settings_list[$setting->getCKey() . '__' . $setting->getNs()] = $setting->getCValue();
+		}
+
+		$form = $this->createFormBuilder($global_settings_list);
+		foreach ($global_settings as $setting) {
+			$form->add($setting->getCKey() . '__' . $setting->getNs(), 'text',
+				array('label' => $setting->getTitle())
+			);
+		}
+
+		// End of global settings Form
+		
+		$domains_availible = DomainsQuery::Create()
+            ->find()
+        ;
+        
+		return $this->render('AdminBundle:Settings:global.html.twig', array(
+			'form'      => $form->getForm()->createView(),
+			'add_global_setting_form' => $form_add_global_setting->createView(),
+			'domains_availible' => $domains_availible
 		));
     }
 
-    public function domainAction()
+    public function domainAction($domain_key = 'COM')
     {
-        return $this->render('AdminBundle:Default:settings.html.twig');
+       	$request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            
+            $data = $request->get('form');
+            foreach ($data as $key_ns => $c_value) {
+
+            	if('_token' == $key_ns) continue;
+
+            	$keys = explode('__',$key_ns);
+            	$c_key = $keys[0];
+            	$ns = $keys[1];
+            	$domain_key = $keys[3];
+
+            	try{
+	            	$setting = DomainsSettingsQuery::create()
+						->filterByNs($ns)
+						->filterByDomainKey($locale)
+						->findOneByCKey($c_key)
+						->setCValue($c_value)
+						->save()
+					;
+				}catch(PropelException $e){
+            		$this->get('session')->setFlash('notice', 'settings.updated.failed.'.$e);
+				}
+            }
+            $this->get('session')->setFlash('notice', 'settings.updated');
+        }
+        
+        $domains_settings = new DomainsSettings();
+        $domains_settings->setDomainKey($domain_key);
+
+		$form_add_domain_setting = $this->createFormBuilder($domains_settings)
+			->add('domain_key', 'text')
+			->add('c_key', 'text')
+			->add('ns', 'text')
+			->add('c_value', 'text')
+			->getForm();
+
+		// Generate the Form for the domain settings
+
+		$domain_settings = DomainsSettingsQuery::create()
+			->filterByDomainKey($domain_key)
+			->orderByNs()
+			->find()
+		;
+
+		$domain_settings_list = array();
+		foreach ($domain_settings as $setting) {
+			$domain_settings_list[$setting->getCKey() . '__' . $setting->getNs() . '__' . $setting->getDomainKey()] = $setting->getCValue();
+		}
+
+		$form = $this->createFormBuilder($domain_settings_list);
+		foreach ($domain_settings as $setting) {
+			$form->add($setting->getCKey() . '__' . $setting->getNs() . '__' . $setting->getDomainKey(), 'text'
+			);
+		}
+
+		// End of domain settings Form
+
+		$domains_availible = DomainsQuery::Create()
+            ->find()
+        ;
+		return $this->render('AdminBundle:Settings:domain.html.twig', array(
+			'form'      => $form->getForm()->createView(),
+			'add_domain_setting_form' => $form_add_domain_setting->createView(),
+			'domains_availible' => $domains_availible
+		));
+    }
+
+    /**
+     * Function to add new setting to the settings tables. Either a domain specific or a global setting
+     * 
+     * @param domain_setting If the setting are a domain specific setting
+     * @todo Lav js til ajax kald... ?
+     * @return json status
+     */
+    public function addSettingAction($domain_setting = false)
+    {
+		$request = $this->getRequest();
+		$data = $request->get('form');
+		$c_key = $data['c_key'];
+		$ns = $data['ns'];
+		$c_value = $data['c_value'];
+    	
+    	if($domain_setting){
+			
+			$domain_key = $data['domain_key'];
+
+    		$setting = new DomainsSettings();
+    		$setting->setDomainKey($domain_key);
+    		$setting->setCKey($c_key);
+    		$setting->setNs($ns);
+    		$setting->setCValue($c_value);
+
+			try {
+				$setting->save();
+			} catch (PropelException $e) {
+            	$this->get('session')->setFlash('notice', 'settings.update.failed');
+
+			}
+			return $this->redirect($this->generateUrl('admin_settingsdomain', 
+				array('domain_key' => $domain_key)
+			));
+
+    	}else{
+
+			$title = $data['title'];
+
+    		$setting = new Settings();
+    		$setting->setCKey($c_key);
+    		$setting->setNs($ns);
+    		$setting->setCValue($c_value);
+    		$setting->setTitle($title);
+
+			try {
+				$setting->save();
+			} catch (PropelException $e) {
+            	$this->get('session')->setFlash('notice', 'settings.update.failed');
+			}
+			return $this->redirect($this->generateUrl('admin_settings'));
+    	}
     }
 }
