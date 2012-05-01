9d1ef3c59d5d37180ccad2f4982f8a7a18f4fd1f
Anders Bryrup <andersbryrup@gmail.com>
2012-05-01 21:38:06 +0200

    First Statistics commit

---
+++ src/Hanzo/Bundle/AdminBundle/Controller/StatisticsController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Hanzo\Bundle\AdminBundle\Controller;
+
+use Hanzo\Core\CoreController;
+
+use Hanzo\Core\Hanzo,
+    Hanzo\Core\Tools;
+
+use Hanzo\Model\OrdersLinesQuery;
+
+class StatisticsController extends CoreController
+{
+    public function indexAction()
+    {
+    	// TODO : domain specific
+        $date_filter = array();
+    	if (!empty($_GET['select-periode'])) { // Select input from form
+    		
+    		switch ($this->getRequest()->get('select-periode', null)) {
+    			case 'thisweek':
+    				$date_filter['min'] = date('d-m-Y',strtotime('Monday this week'));
+		            $start = $date_filter['min'];
+		            $date_filter['max'] = date('d-m-Y',strtotime('+6 day', $date_filter['min']));
+		            $end = $date_filter['max'];
+    				break;
+    			case 'thismonth':
+    				$date_filter['min'] = date('d-m-Y',strtotime('first day of this month'));
+		            $start = $date_filter['min'];
+		            $date_filter['max'] = date('d-m-Y',strtotime('last day of this month'));
+		            $end = $date_filter['max'];
+    				break;
+    		}
+
+    	}elseif (isset($_GET['start']) && isset($_GET['end'])) {
+            $start = $this->getRequest()->get('start', null);
+            $end = $this->getRequest()->get('end', null);
+
+            $date_filter['min'] = strtotime($start);
+            $date_filter['max'] = strtotime($end);
+        }else{
+            $date_filter['min'] = date('d-m-Y', strtotime('-1 month', time() ));
+            $start = $date_filter['min'];
+            $date_filter['max'] = date('d-m-Y', time());
+            $end = $date_filter['max'];
+        
+        }
+
+    	$orders_amount = OrdersLinesQuery::create()
+    		->withColumn('SUM(orders_lines.price)','TotalAmount')
+    		->withColumn('SUM(orders_lines.quantity)','TotalProducts')
+    		->useOrdersQuery()
+    			->withColumn('COUNT(orders.id)','TotalOrders')
+    			->withColumn('DATE(orders.finishedAt)','FinishedAt')
+    			->filterByFinishedAt($date_filter)
+    		->endUse()
+    		->select(array('FinishedAt', 'TotalProducts', 'TotalOrders', 'TotalAmount'))
+    		->groupBy('FinishedAt')
+    		->orderBy('FinishedAt')
+    		->find()
+    	;
+
+        return $this->render('AdminBundle:Statistics:index.html.twig', array(
+            'orders_amount'  => $orders_amount,
+            'start' => $start,
+            'end' => $end
+        ));
+    }
+}
