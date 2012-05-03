f8735f772904475cc89e84cda89f535589b3ed55
Henrik Farre <hf@pompdelux-test.(none)>
2012-05-03 09:50:04 +0200

    Allow access from multiple places, remove restrictions from app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,21 +9,22 @@ $ts = microtime(1);
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-    '90.185.183.84', // Bellcom office
-    '85.236.67.1',
-    '87.104.21.83', // Ulrik @home
-    '192.168.42.52', // Ulrik local
-    '87.104.49.70', // Henrik @home
-    '192.168.0.4', // Henrik local
-    '80.197.249.250', // Heinrich @home
-    '87.51.240.173', // Anders @home
-    '127.0.0.1',
-    '::1',
-))) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
+// Handled in vhost/htaccess
+// if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+//     '90.185.183.84', // Bellcom office
+//     '85.236.67.1',
+//     '87.104.21.83', // Ulrik @home
+//     '192.168.42.52', // Ulrik local
+//     '87.104.49.70', // Henrik @home
+//     '192.168.0.4', // Henrik local
+//     '80.197.249.250', // Heinrich @home
+//     '87.51.240.173', // Anders @home
+//     '127.0.0.1',
+//     '::1',
+// ))) {
+//     header('HTTP/1.0 403 Forbidden');
+//     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+// }
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
