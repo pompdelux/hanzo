1936ae3286f1b42df1f17b64a99d2d76037b7a4f
ulrik nielsen <ulrik@o3.dk>
2012-05-10 12:40:49 +0200

    added form error handler

---
+++ src/Hanzo/Core/FormErrors.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Hanzo\Core;
+
+use Symfony\Component\Form\Form;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+
+class FormErrors
+{
+    protected $allErrors = array();
+
+    protected $translator;
+    protected $form;
+    protected $domain;
+
+    /**
+     * Get a Form error handler
+     *
+     * @param Form       $form       the form object
+     * @param Translator $translator translation object
+     * @param string     $domain     translation domain
+     */
+    public function __construct(Form $form, Translator $translator, $domain = 'messages')
+    {
+        $this->form = $form;
+        $this->translator = $translator;
+        $this->domain = $domain;
+
+        $this->getAllErrors($form);
+    }
+
+    /**
+     * Get form errors as string
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        $out = '<ul class="form-errors">';
+        foreach ($this->allErrors as $key => $errors) {
+            $out .= '<li class="'.$key.'">';
+            foreach ($errors as $error) {
+                $out .= $this->translator->trans($error, array(), $this->domain) . '<br>';
+            }
+            $out = substr($out, 0, -4) . '</li>'."\n";
+        }
+        $out .= '</ul>'."\n";
+
+        return $out;
+    }
+
+    /**
+     * Get form errors as an array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->allErrors;
+    }
+
+    protected function getAllErrors($children = null, $template = true)
+    {
+        $this->getAllFormErrors($children, $template = true);
+        return $this->allErrors;
+    }
+
+    protected function getAllFormErrors($children, $template = true)
+    {
+        foreach ($children as $child) {
+            if ($child->hasErrors()) {
+                $vars = $child->createView()->getVars();
+                $errors = $child->getErrors();
+
+                foreach ($errors as $error) {
+                    $this->allErrors[$vars["name"]][] = $this->convertFormErrorObjToString($error);
+                }
+            }
+
+            if ($child->hasChildren()) {
+                $this->getAllErrors($child);
+            }
+        }
+    }
+
+    protected function convertFormErrorObjToString($error)
+    {
+        $errorMessageTemplate = $error->getMessageTemplate();
+        foreach ($error->getMessageParameters() as $key => $value) {
+            $errorMessageTemplate = str_replace($key, $value, $errorMessageTemplate);
+        }
+
+        return $errorMessageTemplate;
+    }
+}
