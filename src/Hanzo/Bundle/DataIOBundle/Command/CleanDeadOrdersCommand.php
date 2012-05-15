b099c659ab76cefd762c943840e8feaa2fdff602
Henrik Farre <henrik@bellcom.dk>
2012-05-15 15:40:18 +0200

    Clean dead orders

--- src/Hanzo/Bundle/DataIOBundle/Command/CleanDeadOrdersCommand.php
+++ src/Hanzo/Bundle/DataIOBundle/Command/CleanDeadOrdersCommand.php
@@ -18,6 +18,8 @@ use Hanzo\Model\Orders,
 
 use Hanzo\Bundle\PaymentBundle\Dibs\DibsApiCallException;
 
+use Hanzo\Bundle\CheckoutBundle\Event\FilterOrderEvent;
+
 use Exception;
 
 class CleanDeadOrdersCommand extends ContainerAwareCommand
@@ -44,20 +46,110 @@ class CleanDeadOrdersCommand extends ContainerAwareCommand
         $toBeDeleted = array();
 
         $orders = $this->getOrders();
+
         foreach ($orders as $order) 
         {
-            echo "Processing: ".$order->getId()." (".$order->getPaymentGatewayId().")\n";
+            $pgId = $order->getPaymentGatewayId();
+            //echo "Processing: ".$order->getId()." (".$pgId.")\n";
+
             $transId = $this->getTransactionId($order);
-            // TODO: set transaction id on order
+
+            if ( is_null($pgId) && !is_null($transId) )
+            {
+                // No payment gateway id, but an transId
+                try
+                {
+                    $callbackData = $this->dibsApi->call()->callback($order);
+                    if ( isset($callbackData['orderid']) )
+                    {
+                        $order->setPaymentGatewayId($callbackData['orderid']);
+                        $pgId = $callbackData['orderid'];
+                    }
+                }
+                catch (DibsApiCallException $e)
+                {
+                    continue;  
+                }
+            }
 
             if ( is_null($transId) )
             {
-              $toBeDeleted[] = $order;
-              continue;
+                $toBeDeleted[] = $order;
+                continue;
             }
 
-            $status = $this->getStatus( $order );
-            // payinfo
+            $order->setAttribute( 'transact', 'payment', $transId );
+            $order->save();
+
+            try
+            {
+                $status = $this->getStatus( $order );
+            }
+            catch (DibsApiCallException $e)
+            {
+                continue;  
+            }
+
+            if ( isset($status->data['status']) )
+            {
+                switch ($status->data['status']) 
+                {
+                    case 2:
+                        // Looks like payment is ok -> update order
+                        $order->setState( Orders::STATE_PAYMENT_OK );
+                        $order->setFinishedAt(time());
+
+                        $fields = array(
+                            'paytype',
+                            'cardnomask',
+                            'cardprefix',
+                            'acquirer',
+                            'cardexpdate',
+                            'currency',
+                            'ip',
+                            'approvalcode',
+                            'transact',
+                        );
+
+                        try
+                        {
+                            $callbackData = $this->dibsApi->call()->callback($order);
+                        }
+                        catch (DibsApiCallException $e)
+                        {
+                            continue;  
+                        }
+
+                        foreach ($fields as $field)
+                        {
+                            $order->setAttribute( $field , 'payment', $callbackData->data[$field] );
+                        }
+                        $order->save();
+                        $this->getContainer()->get('event_dispatcher')->dispatch('order.payment.collected', new FilterOrderEvent($order));
+                        break;
+
+                    default:
+                        $toBeDeleted[] = $order;
+                        continue;
+                        break;
+                } 
+            }
+        }
+
+        $this->deleteOrders( $toBeDeleted );
+    }
+
+    /**
+     * deleteOrders
+     * @return void
+     * @author Henrik Farre <hf@bellcom.dk>
+     **/
+    public function deleteOrders( Array $toBeDeleted )
+    {
+        foreach ($toBeDeleted as $order) 
+        {
+            //echo "Deleting order: ".$order->getId()."\n";
+            $order->delete();
         }
     }
 
