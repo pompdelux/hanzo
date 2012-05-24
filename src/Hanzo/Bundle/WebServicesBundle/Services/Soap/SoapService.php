f35039228207389ff93db0f6c27e54cc2d9f3fc7
ulrik nielsen <ulrik@o3.dk>
2012-05-24 21:04:44 +0200

    added hanzo in constructor

--- src/Hanzo/Bundle/WebServicesBundle/Services/Soap/SoapService.php
+++ src/Hanzo/Bundle/WebServicesBundle/Services/Soap/SoapService.php
@@ -8,35 +8,38 @@ use Monolog;
 
 class SoapService
 {
-  protected $request;
-  protected $logger;
+    protected $request;
+    protected $logger;
+    protected $hanzo;
 
-  public function __construct(Request $request, $logger)
-  {
-    $this->request  = $request;
-    $this->logger   = $logger;
-    $logger->addDebug('Soap call ... initialized.');
-  }
+    public function __construct(Request $request, $logger)
+    {
+        $this->request  = $request;
+        $this->logger   = $logger;
+        $logger->addDebug('Soap call ... initialized.');
+        $this->hanzo = Hanzo::getInstance();
+    }
 
-  public function exec($service)
-  {
-      $response = new Response();
-      $response->headers->set('Content-Type', 'text/xml; charset=UTF-8');
+    public function exec($service)
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml; charset=UTF-8');
 
-      ob_start();
-      $service->handle();
-      $var = ob_get_contents();
-      //ob_end_clean();
-      $response->setContent(trim($var));
-      return $response;
-  }
+        ob_start();
+        $service->handle();
+        $var = ob_get_contents();
+        //ob_end_clean();
+        $response->setContent(trim($var));
 
-  public static function getDebugInfo()
-  {
-    return array(
-      'xml' => file_get_contents('php://input'),
-      'env' => $_SERVER
-    );
-  }
+        return $response;
+    }
+
+    public static function getDebugInfo()
+    {
+        return array(
+            'xml' => file_get_contents('php://input'),
+            'env' => $_SERVER
+        );
+    }
 
 }
