8ee9c97ba8ffbd374c69519916b5a7be3db59f30
Morten M. Hansen <mortenmh@gmail.com>
2012-05-29 14:21:35 +0200

    permissions

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -23,6 +23,8 @@ set :default_stage, "testing"
 
 # mmh@bellcom.dk: use below to rsync the files instead of git clone. Requires capistrano_rsync_with_remote_cache installed (gem install)
 set :deploy_via,  :rsync_with_remote_cache
+# use other rsync_options. Default is: -az --delete
+set :rsync_options, "-rlptoDzO --delete"
 
 #set :copy_exclude, [".git", "spec"]
 
@@ -55,7 +57,9 @@ namespace :deploy do
 
   desc "Update permissions on shared app logs and web dirs to be group writeable"
   task :update_permissions do
-    run "cd #{shared_path} && chmod g+rwX -R app/config && chmod g+rwX app/logs web"
+    # disabled. not allowed to change permissions on files owned by someone else. Consider using sudo.
+    #run "cd #{shared_path} && chmod g+rwX -R app/config && chmod g+rwX app/logs web"
+    run "cd #{shared_path} && chgrp -R www-data cached-copy"
   end
  
 desc "Create logs and public_html symlinks"
@@ -68,5 +72,4 @@ end
 before 'deploy:restart', 'deploy:symlinks'
 
 after 'deploy', 'deploy:update_permissions'
-
 
