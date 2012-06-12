7eec7f943b61442cf468b12ebf4b765049965f75
ulrik nielsen <ulrik@o3.dk>
2012-06-12 22:35:56 +0200

    discontinued twigstring in favor of env

--- src/Hanzo/Twig/Extension/MapsExtension.php
+++ src/Hanzo/Twig/Extension/MapsExtension.php
@@ -3,6 +3,7 @@
 namespace Hanzo\Twig\Extension;
 
 use \Twig_Extension;
+use \Twig_Environment;
 use \Twig_Function_Method;
 use \Twig_Function_Function;
 use \Twig_Filter_Method;
@@ -15,13 +16,11 @@ use Hanzo\Core\Tools;
 
 class MapsExtension extends Twig_Extension
 {
-    protected $template_dir;
     protected $twig_string;
 
     public function __construct(TwigStringService $twig_string)
     {
         $this->twig_string = $twig_string;
-        $this->template_dir = realpath(__DIR__ . '/../../Bundle/CMSBundle/Resources/views/Twig/') . '/';
     }
 
     /**
@@ -38,43 +37,41 @@ class MapsExtension extends Twig_Extension
     public function getFunctions()
     {
         return array(
-            'geo_zip_code_form' => new Twig_Function_Method($this, 'zip_code_form', array('pre_escape' => 'html', 'is_safe' => array('html'))),
-            'consultants_near_you' => new Twig_Function_Method($this, 'consultants_near_you', array('pre_escape' => 'html', 'is_safe' => array('html'))),
-            'consultants_map' => new Twig_Function_Method($this, 'consultants_map', array('pre_escape' => 'html', 'is_safe' => array('html'))),
+            'geo_zip_code_form' => new Twig_Function_Method($this, 'zip_code_form', array('pre_escape' => 'html', 'is_safe' => array('html'), 'needs_environment' => true)),
+            'consultants_near_you' => new Twig_Function_Method($this, 'consultants_near_you', array('pre_escape' => 'html', 'is_safe' => array('html'), 'needs_environment' => true)),
+            'consultants_map' => new Twig_Function_Method($this, 'consultants_map', array('pre_escape' => 'html', 'is_safe' => array('html'), 'needs_environment' => true)),
         );
     }
 
-    public function zip_code_form($type = 'near', $country = 'Denmark')
+    public function zip_code_form(Twig_Environment $env, $type = 'near', $country = 'Denmark')
     {
-        $template = file_get_contents($this->template_dir . 'zip_form.html.twig');
-        return $this->twig_string->parse($template, array(
+        return $env->render('HanzoCMSBundle:Twig:zip_form.html.twig', array(
             'type' => $type,
             'country' => $country
         ));
     }
 
-    public function consultants_map()
+    public function consultants_map(Twig_Environment $env)
     {
         $hanzo = Hanzo::getInstance();
         $config = $hanzo->getByNs('maps');
 
-        $template = file_get_contents($this->template_dir . 'consultants_map.html.twig');
-        return $this->twig_string->parse($template, array(
+        return $env->render('HanzoCMSBundle:Twig:consultants_map.html.twig', array(
             'settings' => 'lat:' . $config['consultants_map.lat'] . ',lng:' . $config['consultants_map.lng'].', zoom:' . $config['consultants_map.zoom'],
             'language' => $config['consultants_map.language'],
             'height'   => $config['consultants_map.height'],
         ));
     }
 
-    public function consultants_near_you($type = 'near')
+    public function consultants_near_you(Twig_Environment $env, $type = 'near')
     {
         $geoip = Hanzo::getInstance()->container->get('geoip_manager');
         $result = $geoip->lookup();
-        $template = file_get_contents($this->template_dir . 'consultants_near_you.html.twig');
-        return $this->twig_string->parse($template, array(
+
+        return $env->render('HanzoCMSBundle:Twig:consultants_near_you.html.twig', array(
             'type' => $type,
-            'lat' => $result['lat'],
-            'lon' => $result['lon'],
+            'lat' => number_format($result['lat'], 8, '.', ''),
+            'lon' => number_format($result['lon'], 8, '.', ''),
         ));
     }
 }
