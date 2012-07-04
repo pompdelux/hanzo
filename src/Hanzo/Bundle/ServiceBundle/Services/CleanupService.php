2f6e3845023b8ec1cc39789aa5d00bb75bad52dd
ulrik nielsen <ulrik@o3.dk>
2012-07-04 14:20:22 +0200

    service updated

--- src/Hanzo/Bundle/ServiceBundle/Services/CleanupService.php
+++ src/Hanzo/Bundle/ServiceBundle/Services/CleanupService.php
@@ -3,6 +3,7 @@
 namespace Hanzo\Bundle\ServiceBundle\Services;
 
 use Criteria;
+use Propel;
 
 use Hanzo\Core\Hanzo;
 use Hanzo\Core\Tools;
@@ -10,6 +11,7 @@ use Hanzo\Core\Tools;
 use Hanzo\Bundle\PaymentBundle\Dibs\DibsApi;
 
 use Hanzo\Model\Orders;
+use Hanzo\Model\OrdersPeer;
 use Hanzo\Model\OrdersQuery;
 
 class CleanupService
@@ -40,58 +42,79 @@ class CleanupService
     /**
      * delete orders deemed dead
      *
-     * @return int number of deleted orders
+     * @param boolean $dry_run testing flag.
      */
-    public function deleteDeadOrders()
+    public function deleteStaleOrders($dry_run = false)
     {
-        // first we nuke orders, wich:
-        // - have not been touched in the last 2 hours
-        //- and have a negative status
+        Propel::setForceMasterConnection(true);
+
         $orders = OrdersQuery::create()
+            ->joinWithOrdersAttributes()
             ->filterByState(0, Criteria::LESS_THAN)
+            ->filterByState(Orders::STATE_ERROR_PAYMENT, Criteria::GREATER_THAN)
             ->filterByUpdatedAt(date('Y-m-d H:i:s', strtotime('2 hours ago')), Criteria::LESS_THAN)
-            ->find()
-        ;
-
-        if ($count = $orders->count()) {
-            $orders->delete();
-        }
-
-        return $count;
-    }
-
-
-    /**
-     * try to handle orders with failed callbacks
-     *
-     * @return [type] [description]
-     */
-    public function failedPaymentOrders()
-    {
-      return true;
-
-        $orders = OrdersQuery::create()
-            ->filterByState(0, Criteria::GREATER_THAN)
-            ->filterByState(Orders::STATE_SHIPPED, Criteria::LESS_THAN)
-            ->filterByPaymentGatewayId(NULL, Criteria::ISNULL)
-            ->filterByBillingMethod('dibs')
-            ->filterByInEdit(0)
-            ->filterByUpdatedAt(date('Y-m-d H:i:s', strtotime('2 hours ago')), Criteria::LESS_THAN)
+            ->filterByInEdit(false)
             ->find()
         ;
 
         foreach ($orders as $order) {
-            $transaction = OrdersAttributesQuery::create()
-                ->filterByOrdersId($order->getId())
-                ->filterByCKey('transact')
-                ->filterByNS('payment:gateway')
-                ->findOne()
-            ;
+            if (isset($order->getAttributes()->payment) && empty($order->getAttributes()->payment->transact)) {
+                if ($dry_run) {
+                    error_log('['.date('Y-m-d H:i:s').'] Order: #'.$order->getId().' will be deleted, state is: '.$order->getState());
+                    continue;
+                }
 
-            if ($transaction) {
-                $transaction = $transaction->getCValue();
-
+                $order->delete();
             }
         }
+
+        Propel::setForceMasterConnection(false);
+    }
+
+
+    /**
+     * cancel order edits, where people abandon their edit session without releasing the edit lock
+     *
+     * @param  Container $container service container
+     * @param  boolean   $dry_run   testing flag.
+     */
+    public function cancelStaleOrderEdit($container, $dry_run = false)
+    {
+        Propel::setForceMasterConnection(true);
+
+        $orders = OrdersQuery::create()
+            ->filterByInEdit(true)
+            ->filterByUpdatedAt(array('max' => '-3 hours'))
+            ->filterByState(Orders::STATE_PENDING, Criteria::LESS_THAN)
+            ->filterByState(Orders::STATE_ERROR_PAYMENT, Criteria::GREATER_THAN)
+            ->find()
+        ;
+
+        foreach ($orders as $order) {
+            if ($dry_run) {
+                error_log('['.date('Y-m-d H:i:s').'] Order: #'.$order->getId().' will be roled back one version and unlocked in AX.');
+                continue;
+            }
+
+            $order->toPreviousVersion();
+            $container->get('ax_manager')->lockUnlockSalesOrder($order, false);
+        }
+
+        // // should _not_ be necessary, but...
+        // $orders = OrdersQuery::create()
+        //     ->filterByInEdit(true)
+        //     ->filterByState(array(
+        //         Orders::STATE_BEING_PROCESSED,
+        //         Orders::STATE_SHIPPED,
+        //     ))
+        //     ->find()
+        // ;
+
+        // foreach ($orders as $order) {
+        //     $order->setInEdit(false);
+        //     $order->save();
+        // }
+
+        Propel::setForceMasterConnection(false);
     }
 }
