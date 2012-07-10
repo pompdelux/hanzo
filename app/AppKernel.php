7258a1bd5f129e2112dd9660e44760dd67aa3d04
Anders Bryrup <andersbryrup@gmail.com>
2012-07-10 20:41:26 +0200

    Add Hurtig Ordre aka QuickOrderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -45,6 +45,7 @@ class AppKernel extends Kernel
             new Hanzo\Bundle\AdminBundle\AdminBundle(),
             new Hanzo\Bundle\WallBundle\WallBundle(),
             new Hanzo\Bundle\EventsBundle\EventsBundle(),
+            new Hanzo\Bundle\QuickOrderBundle\QuickOrderBundle(),
         );
 
         if (preg_match('/^(test|dev)_/', $this->getEnvironment())) {
