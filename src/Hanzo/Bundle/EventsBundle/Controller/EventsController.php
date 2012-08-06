9475b44cb268660b420a3865aae7263ca31c97ac
ulrik nielsen <ulrik@o3.dk>
2012-08-06 12:58:47 +0200

    error and trans fixing

--- src/Hanzo/Bundle/EventsBundle/Controller/EventsController.php
+++ src/Hanzo/Bundle/EventsBundle/Controller/EventsController.php
@@ -106,7 +106,7 @@ class EventsController extends CoreController
             $event = new Events();
         }
 
-        $form = $this->createFormBuilder($event)
+        $form = $this->createFormBuilder($event, array('translation_domain' => 'events'))
             ->add('customers_id', 'hidden')
             ->add('event_date', 'datetime',
                 array(
@@ -115,17 +115,21 @@ class EventsController extends CoreController
                     'date_format' => 'yyyy-MM-dd hH:im',
                     'attr' => array('class' => 'datetimepicker'),
                     'label' => 'events.event_date.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
                 )
             )->add('host', 'text',
                 array(
                     'label' => 'events.host.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
+                    'invalid_message' => $translator->trans('', array(), 'events')
                 )
             )->add('address_line_1', 'text',
                 array(
                     'label' => 'events.address_line_1.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
                 )
             // )->add('address_line_2', 'text',
             //     array(
@@ -136,22 +140,26 @@ class EventsController extends CoreController
             )->add('postal_code', 'text',
                 array(
                     'label' => 'events.postal_code.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
                 )
             )->add('city', 'text',
                 array(
                     'label' => 'events.city.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
                 )
             )->add('phone', 'text',
                 array(
                     'label' => 'events.phone.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
                 )
             )->add('email', 'text',
                 array(
                     'label' => 'events.email.label',
-                    'translation_domain' => 'events'
+                    'translation_domain' => 'events',
+                    'error_bubbling' => true,
                 )
             )->add('description', 'textarea',
                 array(
@@ -256,7 +264,7 @@ class EventsController extends CoreController
                 $event->save();
 
                 $mailer = $this->get('mail_manager');
-                if ($changed){
+                if ($changed) {
                     if ($changed_host){ // If the event has changed and its a new host, send specific mails to all
                         if ($event->getNotifyHostess()){
 
