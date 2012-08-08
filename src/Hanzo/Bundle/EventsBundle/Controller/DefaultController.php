81213994bbbc46816ffce2054acd6c384655d955
Henrik Farre <henrik@bellcom.dk>
2012-08-08 12:57:30 +0200

    First draft of create customer under events

--- src/Hanzo/Bundle/EventsBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/EventsBundle/Controller/DefaultController.php
@@ -4,12 +4,99 @@ namespace Hanzo\Bundle\EventsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Hanzo\Core\CoreController,
+    Hanzo\Core\Hanzo,
+    Hanzo\Model\Customers,
+    Hanzo\Model\Addresses,
+    Hanzo\Model\CountriesPeer,
+    Hanzo\Model\CustomersQuery,
+    Hanzo\Bundle\AccountBundle\Form\Type\CustomersType,
+    Hanzo\Bundle\AccountBundle\Form\Type\AddressesType
+    ;
 
-class DefaultController extends Controller
+class DefaultController extends CoreController
 {
     
     public function indexAction($name)
     {
         return $this->render('EventsBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * createCustomerAction
+     * @return void
+     * @author Henrik Farre <hf@bellcom.dk>
+     **/
+    public function createCustomerAction()
+    {
+        $hanzo = Hanzo::getInstance();
+
+        $domainKey = $hanzo->get('core.domain_key');
+
+        $errors = '';
+
+        $customer = new Customers();
+        $addresses = new Addresses();
+
+        $countries = CountriesPeer::getAvailableDomainCountries();
+
+        if ( count( $countries ) == 1 ) // for .dk, .se, .no and maybe .nl
+        {
+            $addresses->setCountry( $countries[0]->getLocalName() );
+            $addresses->setCountriesId( $countries[0]->getId() );
+        }
+
+        $customer->addAddresses($addresses);
+
+        $form = $this->createForm(
+            new CustomersType(true, new AddressesType($countries)),
+            $customer,
+            array('validation_groups' => 'customer')
+        );
+
+        return $this->render('EventsBundle:Default:create_customer.html.twig', array(
+            'page_type' => 'events-create-customer',
+            'form' => $form->createView(),
+            'errors' => $errors,
+            'domain_key' => $domainKey
+            ));
+    }
+
+    /**
+     * fetchCustomerAction
+     * @return void
+     * @author Henrik Farre <hf@bellcom.dk>
+     **/
+    public function fetchCustomerAction($type)
+    {
+        $request = $this->getRequest();
+        $value = $request->get('value');
+
+        $error = false;
+
+        switch ($type) 
+        {
+          case 'email':
+              $customer = CustomersQuery::create()
+                  ->findOneByEmail($value);
+
+              if ($customer instanceof Customers) 
+              {
+                  $data = array(
+                      'first_name' => $customer->getFirstName(),
+                      'last_name'  => $customer->getLastName(),
+                      );;
+              }
+              break;
+          case 'phone':
+              // code...
+              break;
+        }
+
+        return $this->json_response(array(
+            'error' => $error,
+            'msg'   => '',
+            'data'  => $data, 
+        ));
     }
 }
--- web/fx/scripts/events.js
+++ web/fx/scripts/events.js
@@ -11,7 +11,60 @@ var calendar = (function($) {
   return pub;
 })(jQuery);
 
+// Used by events create customer
+var events = (function($) {
+  var pub = {};
 
+  pub.create_customer_init = function() {
+      fetch_customer();
+      customers_accept();
+  };
+
+  var fetch_customer = function() {
+    $("#fetch-customer-form").submit(function(e) {
+      e.preventDefault();
+
+      var url = base_url;
+      var data = {
+        value: $("#fetch-customer-form input").val()
+      };
+
+      var reg = new RegExp("^[0-9]+$");
+
+      if ( reg.test(data.value) ) {
+          url += 'events/fetch-customer/phone/';
+      }
+      else {
+          url += 'events/fetch-customer/email/';
+      }
+
+      $.ajax({
+        url: url,
+        type: 'POST',
+        dataType: 'json',
+        data: data,
+        success: function(data) {
+          // do stuff 
+        }
+      });
+    });
+  };
+
+  var customers_accept = function() {
+      if ($('#customers_accept:checked').length) {
+          $('form.create input.button').show();
+      }
+      $('#customers_accept').on('change', function() {
+          if ($(this).prop("checked")) {
+              $('form.create input.button').show();
+          } else {
+              $('form.create input.button').hide();
+          }
+      });
+  };
+
+  return pub;
+})(jQuery);
 
 if ($("#calendar").length) {
   calendar.init();
@@ -32,11 +85,11 @@ $('#body-event #participants a.delete').on('click', function(event) {
 });
 
 $('#find-customer-by-phone-form').submit(function(e){
-	e.preventDefault();
-	$submit = $(this).find('input[type="submit"]');
-	$submit.attr('disabled', true);
-	var phone = $('#find-customer-by-phone').val();
-	$.ajax({
+  e.preventDefault();
+  var $submit = $(this).find('input[type="submit"]');
+  $submit.attr('disabled', true);
+  var phone = $('#find-customer-by-phone').val();
+  $.ajax({
       url: base_url + 'account/nno/' + phone,
       dataType: 'json',
       async: false,
@@ -46,28 +99,28 @@ $('#find-customer-by-phone-form').submit(function(e){
             dialoug.alert(ExposeTranslation.get('js:notice'), response.message);
           }
         } else {
-        	$('#form_host').val(response.data.christianname + ' ' + response.data.surname);
-        	$('#form_address_line_1').val(response.data.address);
-        	$('#form_postal_code').val(response.data.zipcode);
-        	$('#form_city').val(response.data.district);
-        	$('#form_phone').val(response.data.phone);
+          $('#form_host').val(response.data.christianname + ' ' + response.data.surname);
+          $('#form_address_line_1').val(response.data.address);
+          $('#form_postal_code').val(response.data.zipcode);
+          $('#form_city').val(response.data.district);
+          $('#form_phone').val(response.data.phone);
         }
 
         $submit.attr('disabled', false);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         dialoug.error(ExposeTranslation.get('js:notice'), ExposeTranslation.get('js:an.error.occurred'));
-		    $submit.attr('disabled', false);
+        $submit.attr('disabled', false);
       }
     });
 });
 
 $('#find-customer-by-email-form').submit(function(e){
-	e.preventDefault();
-	$submit = $(this).find('input[type="submit"]');
-	$submit.attr('disabled', true);
-	var email = $('#find-customer-by-email').val();
-	$.ajax({
+  e.preventDefault();
+  var $submit = $(this).find('input[type="submit"]');
+  $submit.attr('disabled', true);
+  var email = $('#find-customer-by-email').val();
+  $.ajax({
       url: base_url + 'events/getcustomer/' + encodeURIComponent(email),
       dataType: 'json',
       async: false,
@@ -77,10 +130,10 @@ $('#find-customer-by-email-form').submit(function(e){
             dialoug.alert(ExposeTranslation.get('js:notice'), response.message);
           }
         } else {
-        	$('#form_customers_id').val(response.data.id);
-        	$('#form_host').val(response.data.name);
-        	$('#form_phone').val(response.data.phone);
-        	$('#form_email').val(response.data.email);
+          $('#form_customers_id').val(response.data.id);
+          $('#form_host').val(response.data.name);
+          $('#form_phone').val(response.data.phone);
+          $('#form_email').val(response.data.email);
           $('#form_address_line_1').val(response.data.address);
           $('#form_postal_code').val(response.data.zip);
           $('#form_city').val(response.data.city);
@@ -90,17 +143,17 @@ $('#find-customer-by-email-form').submit(function(e){
       },
       error: function(jqXHR, textStatus, errorThrown) {
         dialoug.error(ExposeTranslation.get('js:notice'), ExposeTranslation.get('js:an.error.occurred'));
-		    $submit.attr('disabled', false);
+        $submit.attr('disabled', false);
       }
     });
 });
 
 $('.delete-event').click(function(e){
-	e.preventDefault();
-	$a = $(this);
-	dialoug.confirm( ExposeTranslation.get('js:notice'), $(this).data('confirm-message'), function(choise) {
+  e.preventDefault();
+  var $a = $(this);
+  dialoug.confirm( ExposeTranslation.get('js:notice'), $(this).data('confirm-message'), function(choise) {
     if (choise === 'ok') {
-    	window.location.href = $a.attr('href');
+      window.location.href = $a.attr('href');
     }
-	});
+  });
 });
