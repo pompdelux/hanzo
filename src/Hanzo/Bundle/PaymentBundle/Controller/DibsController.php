06a8c6f5ab00352b05f16f0ee5b85ac10ef9747d
ulrik nielsen <ulrik@o3.dk>
2012-08-14 10:29:37 +0200

    cleanup

--- src/Hanzo/Bundle/PaymentBundle/Controller/DibsController.php
+++ src/Hanzo/Bundle/PaymentBundle/Controller/DibsController.php
@@ -167,7 +167,7 @@ class DibsController extends CoreController
 
         if ( !empty($order) && $order->getId() !== $this->get('session')->get('order_id') )
         {
-          error_log(__LINE__.':'.__FILE__.' Order id mismatch, in url: '.$order_id. ' in session: '. $this->get('session')->get('order_id') ); // hf@bellcom.dk debugging 
+          error_log(__LINE__.':'.__FILE__.' Order id mismatch, in url: '.$order_id. ' in session: '. $this->get('session')->get('order_id') ); // hf@bellcom.dk debugging
         }
 
         return $this->render('PaymentBundle:Default:process.html.twig');
