a6ed3885afcf49121fcb152ddb61fdeb68b450ca
Anders Bryrup <andersbryrup@gmail.com>
2012-08-31 11:26:27 +0200

    check stock test

--- src/Hanzo/Bundle/QuickOrderBundle/Controller/DefaultController.php
+++ src/Hanzo/Bundle/QuickOrderBundle/Controller/DefaultController.php
@@ -122,12 +122,12 @@ class DefaultController extends CoreController
 
         $result = array();
         if ($products->count()) {
-#            $stock = $this->get('stock');
+            $stock = $this->get('stock');
 #            $stock->prime($products);
             foreach ($products as $product) {
-#                if($stock->check($product)){
+                if($stock->check($product->getId())){
             	   $result[] = $product->getSku();
-#                }
+                }
             }
             if(count($result)){
                 return $this->json_response(array(
