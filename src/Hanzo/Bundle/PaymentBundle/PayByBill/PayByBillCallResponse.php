ce4783a75b4907e645b9e1bb0e516c2244f50846
ulrik nielsen <ulrik@o3.dk>
2012-09-13 14:37:03 +0200

    new files added to support logging order deletion

---
+++ src/Hanzo/Bundle/PaymentBundle/PayByBill/PayByBillCallResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hanzo\Bundle\PaymentBundle\PayByBill;
+
+class PayByBillCallResponse
+{
+    public function isError()
+    {
+        return false;
+    }
+}
---
+++ src/Hanzo/Model/OrdersDeletedLogPeer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hanzo\Model;
+
+use Hanzo\Model\om\BaseOrdersDeletedLogPeer;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'orders_deleted_log' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Hanzo.Model
+ */
+class OrdersDeletedLogPeer extends BaseOrdersDeletedLogPeer {
+
+} // OrdersDeletedLogPeer
