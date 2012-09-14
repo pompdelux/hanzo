294e67b2a033b13eaa349fba75a3c2fb6dfc5a25
Anders Bryrup <andersbryrup@gmail.com>
2012-09-14 12:33:12 +0200

    NB

--- src/Hanzo/Bundle/PaymentBundle/Controller/GothiaController.php
+++ src/Hanzo/Bundle/PaymentBundle/Controller/GothiaController.php
@@ -90,7 +90,7 @@ class GothiaController extends CoreController
 
         // Use form validation?
 
-        if (!is_numeric($SSN) AND $domain_key !== 'NO') { // Should be changed to FI This is TEST
+        if (!is_numeric($SSN) AND $domain_key !== 'NB') {
             // alphanumeric and not in finland
             return $this->json_response(array(
                 'status' => FALSE,
@@ -98,14 +98,14 @@ class GothiaController extends CoreController
             ));
         }
 
-        if ((strlen($SSN) < 10 AND $domain_key !== 'NO') OR strlen($SSN) < 11) {
+        if ((strlen($SSN) < 10 AND $domain_key !== 'NB') OR strlen($SSN) < 11) {
             return $this->json_response(array(
                 'status' => FALSE,
                 'message' => $translator->trans('json.ssn.to_short', array(), 'gothia'),
             ));
         }
 
-        if ((strlen($SSN) > 10 AND $domain_key !== 'NO') OR strlen($SSN) > 11) {
+        if ((strlen($SSN) > 10 AND $domain_key !== 'NB') OR strlen($SSN) > 11) {
             return $this->json_response(array(
                 'status' => FALSE,
                 'message' => $translator->trans('json.ssn.to_long', array(), 'gothia'),
