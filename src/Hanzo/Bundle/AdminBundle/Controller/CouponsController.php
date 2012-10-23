b016c5bbaf47428c6f3727b5b7d383574ba11e96
ulrik nielsen <ulrik@o3.dk>
2012-10-23 12:43:16 +0200

    removed customer part of coupons

--- src/Hanzo/Bundle/AdminBundle/Controller/CouponsController.php
+++ src/Hanzo/Bundle/AdminBundle/Controller/CouponsController.php
@@ -2,17 +2,19 @@
 
 namespace Hanzo\Bundle\AdminBundle\Controller;
 
+use Criteria;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 use Hanzo\Core\Hanzo;
 use Hanzo\Core\CoreController;
 use Hanzo\Core\Tools;
 
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-
-use Hanzo\Model\CouponsQuery,
-	Hanzo\Model\Coupons,
-	Hanzo\Model\CouponsToCustomersQuery,
-    Hanzo\Model\CouponsToCustomers,
-	Hanzo\Model\CustomersQuery;
+use Hanzo\Model\CouponsQuery;
+use Hanzo\Model\Coupons;
+use Hanzo\Model\CouponsToCustomersQuery;
+use Hanzo\Model\CouponsToCustomers;
+use Hanzo\Model\CustomersQuery;
 use Hanzo\Model\DomainsSettingsQuery;
 
 class CouponsController extends CoreController
@@ -23,23 +25,28 @@ class CouponsController extends CoreController
             return $this->redirect($this->generateUrl('admin'));
         }
 
-    	$hanzo = Hanzo::getInstance();
+        $hanzo = Hanzo::getInstance();
         $container = $hanzo->container;
         $route = $container->get('request')->get('_route');
         $router = $container->get('router');
 
-	    $coupons = CouponsQuery::create();
+        $coupons = CouponsQuery::create()
+            ->orderByActiveFrom(Criteria::DESC)
+            ->orderByCreatedAt(Criteria::DESC)
+        ;
 
-    	if (isset($_GET['q'])) {
+        if (isset($_GET['q'])) {
             $q_clean = $this->getRequest()->get('q', null);
             $q = '%'.$q_clean.'%';
 
-	    	$coupons = $coupons->filterByCode($q);
-	    }
+            $coupons->filterByCode($q);
+        } else {
+            $coupons->filterByIsActive(true);
+        }
 
-	    $coupons = $coupons->paginate($pager, 20, $this->getDbConnection());
+        $coupons = $coupons->paginate($pager, 20, $this->getDbConnection());
 
-		$paginate = null;
+        $paginate = null;
         if ($coupons->haveToPaginate()) {
 
             $pages = array();
@@ -82,94 +89,82 @@ class CouponsController extends CoreController
             return $this->redirect($this->generateUrl('admin'));
         }
 
-    	$coupon = null;
-
-    	if ($id) {
-    		$coupon = CouponsQuery::create()
-    			->filterById($id)
-                ->findOne($this->getDbConnection())
-    		;
-    	}else{
-    		$coupon = new Coupons();
-    	}
-
-		$currencies_data = array();
-    	$currencies = DomainsSettingsQuery::create()
-    		->filterByNs('core')
-    		->filterByCKey('currency')
-    		->find($this->getDbConnection())
-    	;
-
-    	foreach ($currencies as $currency) {
-    		$currencies_data[$currency->getCValue()] = $currency->getCValue();
-    	}
-
-    	$form = $this->createFormBuilder($coupon)
-            ->add('code', 'text',
-                array(
-                    'label' => 'admin.coupons.code',
-                    'translation_domain' => 'admin',
-                    'required' => true
-                )
-            )->add('amount', 'number',
-                array(
-                    'label' => 'admin.coupons.amount',
-                    'translation_domain' => 'admin',
-                    'required' => true
-                )
-            )->add('currency_code', 'choice',
-                array(
-                	'choices' => $currencies_data,
-                    'label' => 'admin.coupons.currency',
-                    'translation_domain' => 'admin',
-                    'required' => false
-                )
-            )->add('uses_pr_coupon', 'integer',
-                array(
-                    'label' => 'admin.coupons.uses_pr_coupon',
-                    'translation_domain' => 'admin',
-                    'required' => false
-                )
-            )->add('uses_pr_coustomer', 'integer',
-                array(
-                    'label' => 'admin.coupons.uses_pr_customer',
-                    'translation_domain' => 'admin',
-                    'required' => false
-                )
-            )->add('active_from', 'date',
-                array(
-                	'input' => 'string',
-                	'widget' => 'single_text',
-                	'format' => 'dd-MM-yyyy',
-                    'label' => 'admin.coupons.active_from',
-                    'translation_domain' => 'admin',
-                    'required' => false,
-                    'attr' => array('class' => 'datepicker')
-                )
-            )->add('active_to', 'date',
-                array(
-                	'input' => 'string',
-                	'widget' => 'single_text',
-                	'format' => 'dd-MM-yyyy',
-                    'label' => 'admin.coupons.active_to',
-                    'translation_domain' => 'admin',
-                    'required' => false,
-                    'attr' => array('class' => 'datepicker')
-                )
-            )->getForm()
-        ;
-
-        $customers = CustomersQuery::create()
-            ->find($this->getDbConnection())
-        ;
-
-        $couponstocustomers = CouponsToCustomersQuery::create()
-        	->joinWithCustomers()
-        	->filterByCouponsId($coupon->getId())
-            ->find($this->getDbConnection())
-        ;
-
         $request = $this->getRequest();
+        $coupon = null;
+
+        if ($id) {
+            $coupon = CouponsQuery::create()
+                ->filterById($id)
+                ->findOne($this->getDbConnection())
+            ;
+        } else {
+            $coupon = new Coupons();
+
+            if ('GET' === $request->getMethod()) {
+                // make sure we have a uniq code for every coupon
+                while (true) {
+                    $code = substr(str_shuffle(str_shuffle(str_repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 2))), 0, 9);
+                    $check = CouponsQuery::create()
+                        ->filterByCode($code)
+                        ->findOne()
+                    ;
+
+                    if (empty($check)) {
+                        break;
+                    }
+                }
+                $coupon->setCode($code);
+            }
+        }
+
+        $currencies_data = array();
+        $currencies = DomainsSettingsQuery::create()
+            ->filterByNs('core')
+            ->filterByCKey('currency')
+            ->find($this->getDbConnection())
+        ;
+
+        foreach ($currencies as $currency) {
+            $currencies_data[$currency->getCValue()] = $currency->getCValue();
+        }
+
+        $form = $this->createFormBuilder($coupon)
+            ->add('code', 'text', array(
+                'label' => 'admin.coupons.code',
+                'translation_domain' => 'admin',
+                'required' => true
+            ))->add('amount', 'number', array(
+                'label' => 'admin.coupons.amount',
+                'translation_domain' => 'admin',
+                'required' => true
+            ))->add('currency_code', 'choice', array(
+                'choices' => $currencies_data,
+                'label' => 'admin.coupons.currency',
+                'translation_domain' => 'admin',
+                'required' => false
+            ))->add('active_from', 'date', array(
+                'input' => 'string',
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+                'label' => 'admin.coupons.active_from',
+                'translation_domain' => 'admin',
+                'required' => false,
+                'attr' => array('class' => 'datepicker')
+            ))->add('active_to', 'date', array(
+                'input' => 'string',
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+                'label' => 'admin.coupons.active_to',
+                'translation_domain' => 'admin',
+                'required' => false,
+                'attr' => array('class' => 'datepicker')
+            ))->add('is_active', 'checkbox', array(
+                'label' => 'admin.customer.is_active',
+                'translation_domain' => 'admin',
+                'required' => false,
+            ))->getForm()
+        ;
+
         if ('POST' === $request->getMethod()) {
             $form->bindRequest($request);
 
@@ -183,8 +178,6 @@ class CouponsController extends CoreController
 
         return $this->render('AdminBundle:Coupons:view.html.twig', array(
             'form' => $form->createView(),
-            'couponstocustomers' => $couponstocustomers,
-            'customers' => $customers,
             'coupon' => $coupon,
             'database' => $this->getRequest()->getSession()->get('database')
         ));
@@ -196,7 +189,7 @@ class CouponsController extends CoreController
             throw new AccessDeniedException();
         }
 
-    	$coupon = CouponsQuery::create()
+        $coupon = CouponsQuery::create()
             ->filterById($id)
             ->findOne($this->getDbConnection())
         ;
@@ -206,11 +199,11 @@ class CouponsController extends CoreController
 
 
             if ($this->getFormat() == 'json') {
-	            return $this->json_response(array(
-	                'status' => TRUE,
-	                'message' => $this->get('translator')->trans('delete.coupon.success', array(), 'admin'),
-	            ));
-	        }
+                return $this->json_response(array(
+                    'status' => TRUE,
+                    'message' => $this->get('translator')->trans('delete.coupon.success', array(), 'admin'),
+                ));
+            }
         }
 
         if ($this->getFormat() == 'json') {
@@ -221,77 +214,5 @@ class CouponsController extends CoreController
         }
 
 
-    }
-
-    public function addCustomerAction()
-    {
-        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            throw new AccessDeniedException();
-        }
-
-        $requests = $this->get('request');
-        $coupon_id = $requests->get('coupon');
-        $customer_id = $requests->get('customer');
-
-        $customer = CouponsToCustomersQuery::create()
-            ->filterByCouponsId($coupon_id)
-            ->filterByCustomersId($customer_id)
-            ->findOne($this->getDbConnection())
-        ;
-
-        if (!($customer instanceof CouponsToCustomers)) {
-
-            $customer = new CouponsToCustomers();
-            $customer->setCouponsId($coupon_id);
-            $customer->setCustomersId($customer_id);
-            $customer->save($this->getDbConnection());
-
-            if ($this->getFormat() == 'json') {
-                return $this->json_response(array(
-                    'status' => TRUE,
-                    'message' => $this->get('translator')->trans('add.coupon.customer.success', array(), 'admin'),
-                ));
-            }
-
-        }
-
-        if ($this->getFormat() == 'json') {
-            return $this->json_response(array(
-                'status' => FALSE,
-                'message' => $this->get('translator')->trans('add.coupon.customer.failed', array(), 'admin'),
-            ));
-        }
-    }
-
-    public function deleteCustomerAction($coupon_id, $customer_id)
-    {
-        if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            throw new AccessDeniedException();
-        }
-
-    	$coupon = CouponsToCustomersQuery::create()
-    		->filterByCouponsId($coupon_id)
-            ->filterByCustomersId($customer_id)
-            ->findOne($this->getDbConnection());
-
-        if($coupon instanceof CouponsToCustomers){
-            $coupon->delete($this->getDbConnection());
-
-
-	        if ($this->getFormat() == 'json') {
-	            return $this->json_response(array(
-	                'status' => TRUE,
-	                'message' => $this->get('translator')->trans('delete.coupon.customer.success', array(), 'admin'),
-	            ));
-	        }
-
-        }
-
-        if ($this->getFormat() == 'json') {
-            return $this->json_response(array(
-                'status' => FALSE,
-                'message' => $this->get('translator')->trans('delete.coupon.customer.failed', array(), 'admin'),
-            ));
-        }
     }
 }
