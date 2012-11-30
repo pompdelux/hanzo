bbf44f1ea7cc3be67ba5c4e13700eabf454ab52c
ulrik nielsen <ulrik@o3.dk>
2012-11-30 10:05:53 +0100

    fixed search and restructured code

--- src/Hanzo/Bundle/SearchBundle/DependencyInjection/SearchExtension.php
+++ src/Hanzo/Bundle/SearchBundle/DependencyInjection/SearchExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class HanzoSearchExtension extends Extension
+class SearchExtension extends Extension
 {
     /**
      * {@inheritDoc}
