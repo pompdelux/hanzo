342bd0a0c5cbafdaa4b48f29fe149726b7737842
Anders Bryrup <andersbryrup@gmail.com>
2013-01-11 14:04:08 +0100

    fixed IE8 bug

--- web/fx/2013s1/scripts/social-buttons.js
+++ web/fx/2013s1/scripts/social-buttons.js
@@ -7,7 +7,7 @@
     pub.init = function() {
       $('a.social-button').on('click', function(e){
         e.preventDefault();
-        window.open($(this).attr('href'), 'Share - POMPdeLUX', 'width=728,height=500');
+        window.open($(this).attr('href'), 'POMPdeLUX', 'width=728,height=500');
       });
     };
 
