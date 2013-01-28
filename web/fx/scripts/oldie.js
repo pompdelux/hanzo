14f8e63543f0174fea1ce870ef0d0bc2277609a8
Anders Bryrup <andersbryrup@gmail.com>
2013-01-28 12:24:48 +0100

    Added a placeholder shim

--- web/fx/scripts/oldie.js
+++ web/fx/scripts/oldie.js
@@ -9,5 +9,6 @@
   if ($oldie.length) {
     $('footer li:last-child', $oldie).css('background-image', 'none');
     $('#secondary-links ul li:first-child a', $oldie).css('border-left', 'none');
+    $('input[placeholder], textarea[placeholder]').placeholderEnhanced();
   }
 })(jQuery);
