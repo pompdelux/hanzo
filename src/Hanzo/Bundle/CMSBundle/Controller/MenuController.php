edf9d86b5f58989b4da9fcf80f0dc5aaad0adb50
Anders Bryrup <andersbryrup@gmail.com>
2013-02-04 13:01:48 +0100

    Made the breadcrumb ie7 compatible

--- src/Hanzo/Bundle/CMSBundle/Controller/MenuController.php
+++ src/Hanzo/Bundle/CMSBundle/Controller/MenuController.php
@@ -381,31 +381,36 @@ class MenuController extends CoreController
             $this->menu[$type] .= '<ul class="breadcrumb">';
 
             foreach ($this->trail as $record) {
-                $class = '';
-                $path = $record->getPath();
-                if ($record->getType() == 'frontpage') {
-                    $path = '';
-                }
-
-                if ($record->getTitle()) {
-                    $class = 'inactive';
-                    if ((isset($this->trail[$record->getId()])) ||
-                        ($path == $this->path)
-                    ) {
-                        $class = 'active';
-                    }
-                }
-
-                $class .= ($record === reset($this->trail))?' first':'';
-                $class .= ($record === end($this->trail))?' last':'';
-
-                if (preg_match('~^(f|ht)tps?://~', $path)) {
-                    $uri = $path;
-                } else {
-                    $uri = $this->base_url . '/' . $this->locale . '/' . $path;
-                }
-
                 if($record->getType() !== 'heading'){
+                    $class = '';
+                    $path = $record->getPath();
+                    if ($record->getType() == 'frontpage') {
+                        $path = '';
+                    }
+
+                    if ($record->getTitle()) {
+                        $class = 'inactive';
+                        if ((isset($this->trail[$record->getId()])) ||
+                            ($path == $this->path)
+                        ) {
+                            $class = 'active';
+                        }
+                    }
+                    if($record === reset($this->trail)){ // First
+                        $class .= ' first';
+                    }elseif($record === end($this->trail)){ // Last
+                        $class .= ' last';
+                        $this->menu[$type] .= '<li class="seperator">&gt;</li>';
+                    }else{
+                        $this->menu[$type] .= '<li class="seperator">&gt;</li>';
+                    }
+
+                    if (preg_match('~^(f|ht)tps?://~', $path)) {
+                        $uri = $path;
+                    } else {
+                        $uri = $this->base_url . '/' . $this->locale . '/' . $path;
+                    }
+
                     $this->menu[$type] .= '<li class="' . $class . '"><a href="'. $uri . '" class="page-'.$record->getId().' '.$record->getType().'">' . $record->getTitle() . '</a></li>';
                 }
 
