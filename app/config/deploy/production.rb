f452e799b2e50e739dd1c37f377c54f38bad5aa2
Morten M. Hansen <mmh@bellcom.dk>
2013-02-11 11:27:50 +0100

    new deploy 2.1

--- app/config/deploy/production.rb
+++ app/config/deploy/production.rb
@@ -2,14 +2,25 @@
 # Hanzo / Pompdelux production deploy. 
 #
 
+# needed to get verbose output. -v doesnt work
+logger.level = Logger::MAX_LEVEL
+
 set :deploy_to,   "/var/www/pompdelux" 
 
 set :symfony_env_prods, ["prod_dk", "prod_se", "prod_no", "prod_com", "prod_nl", "prod_fi", "prod_dk_consultant", "prod_se_consultant", "prod_no_consultant", "prod_nl_consultant", "prod_fi_consultant"]
 
 set :adminserver, "pdladmin"
+set :staticserver, "pdlstatic1"
 
 # if we ever get other brances, specify which one to deploy here
 set   :branch, "master"
+
+# use composer for symfony 2.1
+set :use_composer, true
+set :composer_bin, "/usr/local/bin/composer"
+
+# dont delete web/app_* please
+set :clear_controllers, false
 
 #set :update_vendors, true
 set :update_vendors, false
@@ -29,6 +40,9 @@ end
 
 # our redis server. clear cache here
 role :redis, adminserver, :primary => true
+
+# our redis server. clear cache here
+role :static, staticserver, :primary => true
 
 # where to run migrations. :db is also a default and may be used internally.
 role :db, adminserver, :primary => true  # where to run migrations
@@ -60,4 +74,24 @@ namespace :deploy do
   end
 end
 
+# overridden because of chmod without sudo, and loop envinroments
+namespace :symfony do
+  namespace :cache do
+    [:clear, :warmup].each do |action|
+      desc "Cache #{action.to_s}"
+      task action, :roles => :app, :except => { :no_release => true } do
+        case action
+        when :clear
+          capifony_pretty_print "--> Clearing cache"
+        when :warmup
+          capifony_pretty_print "--> Warming up cache"
+        end
 
+        symfony_env_prods.each do |i|
+          run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} cache:#{action.to_s} --env=#{i}'"
+        end
+        capifony_puts_ok
+      end
+    end
+  end
+end
