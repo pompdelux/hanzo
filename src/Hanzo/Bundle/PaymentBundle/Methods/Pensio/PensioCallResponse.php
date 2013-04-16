05c99120edde2f529f58185d692f2ad6fb781fd2
ulrik nielsen <ulrik@o3.dk>
2013-04-16 14:21:17 +0200

    added response headers to log

--- src/Hanzo/Bundle/PaymentBundle/Methods/Pensio/PensioCallResponse.php
+++ src/Hanzo/Bundle/PaymentBundle/Methods/Pensio/PensioCallResponse.php
@@ -9,14 +9,18 @@ class PensioCallResponse
     protected $is_error = false;
     protected $error_message = '';
     protected $xml = '';
+    protected $headers;
 
     /**
      * constructor
      *
-     * @param SimpleXMLElement $xml xml response object
+     * @param array            $headers http_response_header see: http://php.net/manual/en/reserved.variables.httpresponseheader.php
+     * @param SimpleXMLElement $xml     xml response object
      */
-    public function __construct(SimpleXMLElement $xml)
+    public function __construct($headers, SimpleXMLElement $xml)
     {
+        $this->headers = $headers;
+
         if (((string) $xml->Header[0]->ErrorCode[0]) != '0') {
             $this->is_error = true;
             $this->error_message = (string) $xml->Header[0]->ErrorMessage[0];
@@ -45,11 +49,12 @@ class PensioCallResponse
     public function debug()
     {
         return [
-            'status' => !$this->is_error,
-            'reason' => $this->error_message,
-            'status_is_error' => $this->is_error,
-            'status_description' => $this->error_message,
+            'headers' => $this->headers,
             'raw_response' => $this->xml,
+            'reason' => $this->error_message,
+            'status' => !$this->is_error,
+            'status_description' => $this->error_message,
+            'status_is_error' => $this->is_error,
         ];
     }
 }
