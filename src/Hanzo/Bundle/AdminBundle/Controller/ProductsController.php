41807e87057e2b455d165a7ebdea813a397f78c8
ulrik nielsen <ulrik@o3.dk>
2013-06-26 13:26:37 +0200

    fixed missind dbConnection

--- src/Hanzo/Bundle/AdminBundle/Controller/ProductsController.php
+++ src/Hanzo/Bundle/AdminBundle/Controller/ProductsController.php
@@ -249,7 +249,7 @@ class ProductsController extends CoreController
             ->joinWithDomains()
             ->orderByProductsId()
             ->orderByFromDate()
-            ->find();
+            ->find($this->getDbConnection());
 
         $prices = [];
         foreach ($prices_result as $price) {
