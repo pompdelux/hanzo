a4b212a0f454eb070ccd953de9506d3810d6bec6
ulrik nielsen <ulrik@o3.dk>
2013-07-17 08:55:34 +0200

    added try catch to handle unknown requests better

--- src/Hanzo/Bundle/MunerisBundle/Controller/MaxMindController.php
+++ src/Hanzo/Bundle/MunerisBundle/Controller/MaxMindController.php
@@ -3,6 +3,7 @@
 namespace Hanzo\Bundle\MunerisBundle\Controller;
 
 use Hanzo\Core\CoreController;
+use Guzzle\Http\Exception\ClientErrorResponseException;
 
 class MaxMindController extends CoreController
 {
@@ -11,10 +12,20 @@ class MaxMindController extends CoreController
         $start = (microtime(true) * 1000);
         $service = $this->get('muneris.maxmind');
 
+        try {
+            $result = $service->lookup($ip);
+        } catch (ClientErrorResponseException $e) {
+            return $this->json_response([
+                'status'  => false,
+                'message' => '',
+                '_time'   => (int) ((microtime(true) * 1000) - $start) .'ms',
+            ]);
+        }
+
         return $this->json_response([
             'status'  => true,
             'message' => '',
-            'data'    => $service->lookup($ip),
+            'data'    => $result,
             '_time'   => (int) ((microtime(true) * 1000) - $start) .'ms',
         ]);
     }
--- src/Hanzo/Bundle/MunerisBundle/Controller/NnoController.php
+++ src/Hanzo/Bundle/MunerisBundle/Controller/NnoController.php
@@ -3,6 +3,7 @@
 namespace Hanzo\Bundle\MunerisBundle\Controller;
 
 use Hanzo\Core\CoreController;
+use Guzzle\Http\Exception\ClientErrorResponseException;
 
 class NnoController extends CoreController
 {
@@ -14,13 +15,20 @@ class NnoController extends CoreController
         $request = $guzzle->get('/nno/numbers/'.$number);
         $request->setHeader('Accept', 'application/json');
 
-        $response = $request->send();
-        $body     = $response->getBody();
+        try {
+            $response = $request->send();
+        } catch (ClientErrorResponseException $e) {
+            return $this->json_response([
+                'status'  => false,
+                'message' => $this->get('translator')->trans('create.nno.no_address_found', [], 'account'),
+                '_time'   => (int) ((microtime(true) * 1000) - $start) .'ms',
+            ]);
+        }
 
         return $this->json_response([
             'status'  => true,
             'message' => '',
-            'data'    => json_decode($body),
+            'data'    => json_decode($response->getBody()),
             '_time'   => (int) ((microtime(true) * 1000) - $start) .'ms',
         ]);
     }
