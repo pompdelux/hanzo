15aedcef28742e12f7b17ff0e71f2b5ac4c1ac3f
ulrik nielsen <ulrik@o3.dk>
2013-08-12 10:50:37 +0200

    switches to fuzzy search for postal codes

--- src/Hanzo/Bundle/MunerisBundle/Controller/GeoPostalController.php
+++ src/Hanzo/Bundle/MunerisBundle/Controller/GeoPostalController.php
@@ -61,6 +61,20 @@ class GeoPostalController extends CoreController
      */
     protected function getByName($country, $city)
     {
+        $country = strtolower($country);
+        $city    = strtolower($city);
+
+        // we need to remap some cities, for some reason - guess people do not know where they live...
+        $remap = [
+            'nl' => [
+                'lattrop' => 'lattrop-breklenkamp'
+            ],
+        ];
+
+        if (isset($remap[$country][$city])) {
+            $city = $remap[$country][$city];
+        }
+
         $client = $this->guzzle->get(strtr('/gpc/countries/{country}/fuzies/{city}', [
             '{country}' => $country,
             '{city}'    => $city
