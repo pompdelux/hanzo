83c96e6c3735b5d5b40bf78a039e6d5de72a16ac
Anders Bryrup <andersbryrup@gmail.com>
2013-11-21 14:42:20 +0100

    Always listen on keypress in quickorder. Why shouldnt we?
    This also resolves an issue where er didnt have an class of IE.
    Fixes #270

--- web/fx/scripts/bootstrap-typeahead.js
+++ web/fx/scripts/bootstrap-typeahead.js
@@ -203,9 +203,7 @@
         .on('keypress', $.proxy(this.keypress, this))
         .on('keyup',    $.proxy(this.keyup, this))
 
-      if (/Chrome/.test(navigator.userAgent) || /Safari/.test(navigator.userAgent) || $('html').hasClass('ie')) {
         this.$element.on('keydown', $.proxy(this.keypress, this))
-      }
 
       this.$menu
         .on('click', $.proxy(this.click, this))
