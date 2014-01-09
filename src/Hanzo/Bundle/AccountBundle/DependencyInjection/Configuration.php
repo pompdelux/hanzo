acff8747042aad68dde0e27750880116baca783c
ulrik nielsen <ulrik@o3.dk>
2014-01-09 12:57:01 +0100

    handle consignor configurations

--- src/Hanzo/Bundle/AccountBundle/DependencyInjection/Configuration.php
+++ src/Hanzo/Bundle/AccountBundle/DependencyInjection/Configuration.php
@@ -20,6 +20,42 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('account');
 
+        $rootNode
+            ->children()
+                ->arrayNode('consignor')
+                    ->isRequired()
+                    ->children()
+                        ->scalarNode('trackntrace_url')
+                            ->defaultValue('http://myconsignor.no/WebShopPackageTracker.aspx?installationID=:installation_id:&ActorID=:actor_id:&orderNUmber=:order_id:')
+                        ->end()
+                        ->scalarNode('installation_id')
+                            ->defaultValue('')
+                        ->end()
+                        ->scalarNode('actor_id')
+                            ->defaultValue('')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+
+        /*
+
+
+        installationID
+        90290000026
+
+        ActorID
+        POMPdeLUX DK 63
+        POMPdeLUX NO 66
+        POMPdeLUX SE 64
+        POMPdeLUX NL 67
+        POMPdeLUX FI 68
+        POMPdeLUX DE 69
+
+        */
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
