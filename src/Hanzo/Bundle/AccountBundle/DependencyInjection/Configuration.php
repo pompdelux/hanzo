49db07f51da6d9220422d8e2d8294f9db666890b
ulrik nielsen <ulrik@o3.dk>
2014-01-09 13:47:52 +0100

    cleanup

--- src/Hanzo/Bundle/AccountBundle/DependencyInjection/Configuration.php
+++ src/Hanzo/Bundle/AccountBundle/DependencyInjection/Configuration.php
@@ -39,27 +39,6 @@ class Configuration implements ConfigurationInterface
             ->end()
         ;
 
-
-        /*
-
-
-        installationID
-        90290000026
-
-        ActorID
-        POMPdeLUX DK 63
-        POMPdeLUX NO 66
-        POMPdeLUX SE 64
-        POMPdeLUX NL 67
-        POMPdeLUX FI 68
-        POMPdeLUX DE 69
-
-        */
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
