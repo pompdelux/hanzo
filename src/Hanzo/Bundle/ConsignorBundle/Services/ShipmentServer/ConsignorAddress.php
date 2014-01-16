6480ea6f3fb17a8aa67843c425d88c3cf4f33111
ulrik nielsen <ulrik@o3.dk>
2014-01-16 09:46:12 +0100

    extended address object

--- src/Hanzo/Bundle/ConsignorBundle/Services/ShipmentServer/ConsignorAddress.php
+++ src/Hanzo/Bundle/ConsignorBundle/Services/ShipmentServer/ConsignorAddress.php
@@ -17,12 +17,22 @@ class ConsignorAddress
         'Attention',
         'City',
         'CountryCode',
+        'Email',
         'Kind',
         'Name1',
+        'Phone',
         'PostCode',
         'Street1',
         'Street2',
     ];
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->address;
+    }
 
     /**
      * Construct a new address
@@ -34,22 +44,16 @@ class ConsignorAddress
      * @param string $PostCode
      * @param string $City
      * @param string $CountryCode
+     * @param string $Email
+     * @param string $Phone
      * @param string $Attention
      */
-    public function __construct($Kind = 1, $Name1, $Street1, $Street2, $PostCode, $City, $CountryCode, $Attention = '')
+    public function __construct($Kind = 1, $Name1, $Street1, $Street2, $PostCode, $City, $CountryCode, $Email = '', $Phone = '', $Attention = '')
     {
         foreach ($this->address_keys as $key) {
             if (!empty($$key)) {
                 $this->address[$key] = $$key;
             }
         }
-    }
-
-    /**
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->address;
     }
 }
