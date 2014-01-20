38904a4c19a1ab40a1fe968cf3bb34767904279e
ulrik nielsen <ulrik@o3.dk>
2014-01-20 13:12:50 +0100

    added testing command to stock bundle

---
+++ src/Hanzo/Bundle/StockBundle/Command/MigrateStockToRedisCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Hanzo\Bundle\StockBundle\Command;
+
+
+use Hanzo\Model\ProductsQuery;
+use Hanzo\Model\ProductsStockQuery;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateStockToRedisCommand extends ContainerAwareCommand
+{
+
+    protected $errors = [];
+
+    protected function configure()
+    {
+        $this->setName('hanzo:stock:migrate-to-redis')
+            ->setDescription('Migrate stock to redis from db.')
+        ;
+    }
+
+
+    /**
+     * executes the job
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $stock = $this->getContainer()->get('stock');
+        $response = $stock->decrease(ProductsQuery::create()->findOneById(2), 385);
+var_dump($response);
+        return;
+
+
+
+        $products = ProductsStockQuery::create()
+            ->orderByProductsId()
+            ->orderByAvailableFrom()
+            ->find()
+        ;
+
+        foreach ($products as $product) {
+            $stock->set($product->getProductsId(), $product->getAvailableFrom(), $product->getQuantity());
+        }
+    }
+}
