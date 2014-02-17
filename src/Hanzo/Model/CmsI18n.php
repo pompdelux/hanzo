4e25f0ad0a40d8eb44396a45f96a1222fff92361
ulrik nielsen <ulrik@o3.dk>
2014-02-17 13:27:59 +0100

    added settings handeling

--- src/Hanzo/Model/CmsI18n.php
+++ src/Hanzo/Model/CmsI18n.php
@@ -2,13 +2,15 @@
 
 namespace Hanzo\Model;
 
+use Hanzo\Core\Hanzo;
+use Hanzo\Core\Tools;
 use Hanzo\Model\om\BaseCmsI18n;
 
 
 /**
  * Skeleton subclass for representing a row from the 'cms_i18n' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -16,6 +18,26 @@ use Hanzo\Model\om\BaseCmsI18n;
  *
  * @package    propel.generator.home/un/Documents/Arbejde/Pompdelux/www/hanzo/Symfony/src/Hanzo/Model
  */
-class CmsI18n extends BaseCmsI18n {
+class CmsI18n extends BaseCmsI18n
+{
+    public function getSettings($raw = true)
+    {
+        $translator = Hanzo::getInstance()->container->get('translator');
+        $test       = $this->getId().'.settings';
+        $settings   = trim($translator->trans($test, [], 'cms', $this->getLocale()));
 
+        if ($test == $settings) {
+            $settings = parent::getSettings();
+        }
+
+        if ($raw) {
+            return $settings;
+        }
+
+        if (is_scalar($settings) && substr($settings, 0, 1) == '{') {
+            $settings = json_decode(stripcslashes($settings));
+        }
+
+        return $settings;
+    }
 } // CmsI18n
