ef8c7a77789c9b0688347475040400c5a3b83da9
ulrik nielsen <ulrik@o3.dk>
2014-02-26 14:57:46 +0100

    added a command to allow us to debug stock levels easiere

---
+++ src/Hanzo/Bundle/StockBundle/Command/CheckStockLevelCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Hanzo\Bundle\StockBundle\Command;
+
+use Hanzo\Model\ProductsQuery;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CheckStockLevelCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('hanzo:stock:check-level')
+            ->setDescription('Allows for easy stock overview of an entire style.')
+            ->addArgument('sku', InputArgument::REQUIRED, 'Master SKU to lookup, must be the master record.')
+        ;
+    }
+
+    /**
+     * executes the job
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $stock = $this->getContainer()->get('stock');
+
+        $products = ProductsQuery::create()
+            ->filterByMaster($input->getArgument('sku'))
+            ->orderBySku()
+            ->find()
+        ;
+
+        $stock->prime($products);
+
+        $table = $this->getHelper('table');
+        $table->setHeaders(['SKU', 'date', 'quantity']);
+
+        foreach ($products as $product) {
+            foreach ($stock->get($product, true) as $level)
+            $table->addRow([
+                $product->getSku(),
+                $level['date'],
+                $level['quantity']
+            ]);
+        }
+
+        $table->render($output);
+    }
+}
