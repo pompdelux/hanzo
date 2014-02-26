b0f38d306cbec91ec470023e25740a960bb4e55c
ulrik nielsen <ulrik@o3.dk>
2014-02-26 14:13:50 +0100

    streamlined google tracking codes and added confogurations to the bundle

--- src/Hanzo/Bundle/GoogleBundle/DependencyInjection/Configuration.php
+++ src/Hanzo/Bundle/GoogleBundle/DependencyInjection/Configuration.php
@@ -20,6 +20,20 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('google');
 
+        $rootNode
+            ->children()
+                ->scalarNode('analytics_code')->defaultNull()->end()
+                ->scalarNode('conversion_id')->defaultNull()->end()
+                ->arrayNode('site_verification')
+                    ->prototype('scalar')->end()
+                    ->beforeNormalization()
+                        ->ifString()
+                        ->then(function($v) { return [$v]; })
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
