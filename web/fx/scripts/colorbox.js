6685c3de46eeeb15bc67086ef31016890a65ebe1
Anders Bryrup <andersbryrup@gmail.com>
2014-03-10 11:57:45 +0100

    Add a close sprite class. Use it on colorboxes.

--- web/fx/scripts/colorbox.js
+++ web/fx/scripts/colorbox.js
@@ -1,6 +1,6 @@
 (function ($) {
     $('a.colorbox, a[rel="colorbox"]').colorbox();
     $(document).bind('cbox_load', function () {
-        $('#cboxClose').addClass('sprite delete');
+        $('#cboxClose').addClass('sprite close');
     });
 })(jQuery);
