e7ecdf41060474f1f2fc8a4ed67ddab7a399f6df
ulrik nielsen <ulrik@o3.dk>
2014-04-03 12:00:58 +0200

    added a way to get order edit stats - these are mailed when done.

--- src/Hanzo/Bundle/DataIOBundle/Command/EditUsageCommand.php
+++ src/Hanzo/Bundle/DataIOBundle/Command/EditUsageCommand.php
@@ -53,11 +53,46 @@ class EditUsageCommand extends ContainerAwareCommand
     {
         $orders = OrdersQuery::create()
             ->filterByVersionId(1, \Criteria::GREATER_THAN)
-            ->useOrdersVersionsQuery(null, \Criteria::JOIN)
-                ->filterByOrdersId()
-            ->endUse()
+            ->filterByCreatedAt(strtotime('2014-20-02 00:00:01'), \Criteria::GREATER_THAN)
+            ->filterByCreatedAt(strtotime('2014-17-03 00:00:01'), \Criteria::LESS_THAN)
+            ->filterByState(Orders::STATE_PENDING, \Criteria::GREATER_EQUAL)
+            ->joinWithOrdersLines()
+            ->find()
         ;
 
-        
+        file_put_contents('/tmp/os.csv', "'created at';'original total';'last update at';'current total'\r\n");
+        foreach ($orders as $order) {
+            /** @var Orders $order */
+            /** @var Orders $old_version */
+
+            $old_version = unserialize(OrdersVersionsQuery::create()
+                ->filterByOrdersId($order->getId())
+                ->orderByVersionId(\Criteria::DESC)
+                ->limit(1)
+                ->find()
+                ->getFirst()->getContent()
+            );
+
+            $old_total = 0;
+            foreach ($old_version['products'] as $line) {
+                $old_total += ($line['Price'] * $line['Quantity']);
+            }
+
+            $data = $order->getId().';'
+                . "'".$old_version['order']['CreatedAt']."';"
+                . number_format($old_total, 2, ',', '').';'
+                . "'".$order->getUpdatedAt('Y-m-d H:i:s')."';"
+                . number_format($order->getTotalPrice(), 2, ',', '')."\r\n"
+            ;
+            file_put_contents('/tmp/os.csv', $data, FILE_APPEND);
+        }
+
+        $mailer = $this->getContainer()->get('mail_manager');
+        $mailer->setTo(['hd@pompdelux.dk','un@bellcom.dk']);
+        $mailer->setFrom('mail@pompdelux.dk');
+        $mailer->setSubject('et stk ordrefil');
+        $mailer->setBody("Hej der,\n\nSå er der tal at se på..\n\n-- \nmvh robotten\n");
+        $mailer->addAttachment('/tmp/os.csv');
+        $mailer->send();
     }
 }
