5c1658a7894cdb4bd7db0d8f91e0dcb4ab04f6a5
Anders Bryrup <andersbryrup@gmail.com>
2014-04-09 13:31:53 +0200

    Add table cms_revision.

---
+++ src/Hanzo/Model/map/CmsRevisionTableMap.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Hanzo\Model\map;
+
+use \RelationMap;
+use \TableMap;
+
+
+/**
+ * This class defines the structure of the 'cms_revision' table.
+ *
+ *
+ *
+ * This map class is used by Propel to do runtime db structure discovery.
+ * For example, the createSelectSql() method checks the type of a given column used in an
+ * ORDER BY clause to know whether it needs to apply SQL to make the ORDER BY case-insensitive
+ * (i.e. if it's a text column type).
+ *
+ * @package    propel.generator.src.Hanzo.Model.map
+ */
+class CmsRevisionTableMap extends TableMap
+{
+
+    /**
+     * The (dot-path) name of this class
+     */
+    const CLASS_NAME = 'src.Hanzo.Model.map.CmsRevisionTableMap';
+
+    /**
+     * Initialize the table attributes, columns and validators
+     * Relations are not initialized by this method since they are lazy loaded
+     *
+     * @return void
+     * @throws PropelException
+     */
+    public function initialize()
+    {
+        // attributes
+        $this->setName('cms_revision');
+        $this->setPhpName('CmsRevision');
+        $this->setClassname('Hanzo\\Model\\CmsRevision');
+        $this->setPackage('src.Hanzo.Model');
+        $this->setUseIdGenerator(false);
+        // columns
+        $this->addPrimaryKey('id', 'Id', 'INTEGER', true, null, null);
+        $this->addPrimaryKey('created_at', 'CreatedAt', 'TIMESTAMP', true, null, null);
+        $this->addColumn('publish_on_date', 'PublishOnDate', 'TIMESTAMP', false, null, null);
+        $this->addColumn('revision', 'Revision', 'OBJECT', true, null, null);
+        // validators
+    } // initialize()
+
+    /**
+     * Build the RelationMap objects for this table relationships
+     */
+    public function buildRelations()
+    {
+    } // buildRelations()
+
+    /**
+     *
+     * Gets the list of behaviors registered for this table
+     *
+     * @return array Associative array (name => parameters) of behaviors
+     */
+    public function getBehaviors()
+    {
+        return array(
+            'timestampable' =>  array (
+  'create_column' => 'created_at',
+  'update_column' => 'updated_at',
+  'disable_updated_at' => 'true',
+),
+        );
+    } // getBehaviors()
+
+} // CmsRevisionTableMap
