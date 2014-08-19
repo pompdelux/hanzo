14ca6dc0b4c237c94af9675e026b3090df9e4eaf
ulrik nielsen <ulrik@o3.dk>
2014-08-19 11:16:49 +0200

    re enabled newsletter popup on front page

--- web/fx/scripts/newsletter.js
+++ web/fx/scripts/newsletter.js
@@ -1,165 +1,173 @@
 /*global base_url:true*/
-var newsletter = (function($) {
-  var pub = {
-    lists: {}
-  };
+var newsletter = (function ($) {
+    var pub = {
+        lists: {}
+    };
 
-  var is_initialized = false;
+    var is_initialized = false;
 
-  var newsletter_jsonp_url;
-  var $selector = '';
-  var selectorName = '';
+    var newsletter_jsonp_url;
+    var $selector    = '';
+    var selectorName = '';
 
-  pub.init = function( url ) {
-    if ( is_initialized ) {
-      return;
-    }
-    newsletter_jsonp_url = url;
-    selectorName = "#newsletter-lists-container";
-    $selector = $( selectorName );
-    attachEvents();
-    is_initialized = true;
-  };
-
-  pub.reset = function() {
-    is_initialized = false;
-    newsletter_jsonp_url = null;
-    $selector = '';
-    selectorName = '';
-  };
-
-  function subscriptionsUpdate( email, lists, action ) {
-    dialoug.loading( selectorName, Translator.trans('please.wait') );
-
-    $.getJSON(newsletter_jsonp_url, {method: 'subscriptions:'+action, email: email, lists: lists}, function(data) {
-      if ( data.is_error ) {
-        dialoug.error( Translator.trans('an.error.occurred'), Translator.trans(''+data.content.msg, { 'email': email }) );
-      }
-      else {
-        $.post(base_url+'newsletter/'+action, {email:email}); // send mail to customer
-        dialoug.notice( Translator.trans('action.completed' ), 'info', 2000 );
-      }
-      dialoug.stopLoading();
-    });
-  }
-
-  function attachEvents() {
-    $(selectorName+" form input[type=submit]").click(function(event) {
-      event.preventDefault();
-
-      var lists = [];
-      var action = $( this ).data('action');
-      var email = getEmail();
-
-      $selector.find('.newsletter-list-id').each(function() {
-        if ( $(this).attr('type') === 'checkbox' && $(this).is(":checked") ) {
-          lists.push($(this).val());
+    pub.init = function (url) {
+        if (is_initialized) {
+            return;
         }
-        else if( $(this).attr('type') !== 'checkbox' ) {
-          lists.push($(this).val());
-        }
-      });
+        newsletter_jsonp_url = url;
+        selectorName         = "#newsletter-lists-container";
+        $selector            = $(selectorName);
+        attachEvents();
+        is_initialized = true;
+    };
 
-      subscriptionsUpdate( email, lists, action);
-      lists = [];
-    });
-  }
+    pub.reset = function () {
+        is_initialized       = false;
+        newsletter_jsonp_url = null;
+        $selector            = '';
+        selectorName         = '';
+    };
 
-  function getEmail() {
-    return $selector.find('.newsletter-subscriber-email').val();
-  }
+    function subscriptionsUpdate(email, lists, action) {
+        dialoug.loading(selectorName, Translator.trans('please.wait'));
 
-  pub.lists.get = function( id ) {
-    dialoug.loading( selectorName, Translator.trans('please.wait') );
-    var email = getEmail();
-
-    $.getJSON(newsletter_jsonp_url, {method: 'lists:get', email: email}, function(data) {
-      if ( data.is_error ) {
-        dialoug.error( Translator.trans('an.error.occurred'), data.content.msg  );
-      }
-      else {
-        var $el = $(selectorName+' ul');
-        var list_container = '<ul>';
-
-        $.each(data.content.lists, function(index,list) {
-          if ( ( id !== undefined && id == list.id ) || id === undefined ) { /* must use == to compare id and list.id */
-            list_container += '<li><input type="checkbox" class="newsletter-list-id" name="listid[]" value="'+list.id+'" '+ (list.is_subscribed ? 'checked' : '') +'> '+list.name+'</li>';
-          }
+        $.getJSON(newsletter_jsonp_url, {method: 'subscriptions:' + action, email: email, lists: lists}, function (data) {
+            if (data.is_error) {
+                dialoug.error(Translator.trans('an.error.occurred'), Translator.trans('' + data.content.msg, { 'email': email }));
+            }
+            else {
+                $.post(base_url + 'newsletter/' + action, {email: email}); // send mail to customer
+                dialoug.notice(Translator.trans('action.completed'), 'info', 2000);
+            }
+            dialoug.stopLoading();
         });
+    }
 
-        list_container += '</ul>';
+    function attachEvents() {
+        $(selectorName + " form input[type=submit]").click(function (event) {
+            event.preventDefault();
 
-        $el.replaceWith(list_container);
-      }
-      dialoug.stopLoading();
-    });
-  };
+            var lists  = [];
+            var action = $(this).data('action');
+            var email  = getEmail();
 
-  pub.footer = function() {
-    $('footer .footer-menu-4 form').on('submit', function(event) {
-      event.preventDefault();
+            $selector.find('.newsletter-list-id').each(function () {
+                if ($(this).attr('type') === 'checkbox' && $(this).is(":checked")) {
+                    lists.push($(this).val());
+                }
+                else if ($(this).attr('type') !== 'checkbox') {
+                    lists.push($(this).val());
+                }
+            });
 
-      var $form = $(this);
-      $('input[type="email"]', $form).removeClass('error');
+            subscriptionsUpdate(email, lists, action);
+            lists = [];
+        });
+    }
 
-      $.post(this.action, $form.serialize(), function(response) {
-        var type = (response.status? 'info' : 'error');
-        dialoug.notice(response.message, type, 3000, $form);
+    function getEmail() {
+        return $selector.find('.newsletter-subscriber-email').val();
+    }
 
-        if (response.status) {
-          $('input[type="text"], input[type="email"]', $form).val('');
-        } else {
-          $('input[type="email"]', $form).addClass('error');
+    pub.lists.get = function (id) {
+        dialoug.loading(selectorName, Translator.trans('please.wait'));
+        var email = getEmail();
+
+        $.getJSON(newsletter_jsonp_url, {method: 'lists:get', email: email}, function (data) {
+            if (data.is_error) {
+                dialoug.error(Translator.trans('an.error.occurred'), data.content.msg);
+            }
+            else {
+                var $el = $(selectorName + ' ul');
+                var list_container = '<ul>';
+
+                $.each(data.content.lists, function (index, list) {
+                    if ((id !== undefined && id == list.id) ||
+                        (id === undefined)
+                    ) {
+                        list_container += '<li><input type="checkbox" class="newsletter-list-id" name="listid[]" value="' + list.id + '" ' + (list.is_subscribed ? 'checked' : '') + '> ' + list.name + '</li>';
+                    }
+                });
+
+                list_container += '</ul>';
+
+                $el.replaceWith(list_container);
+            }
+            dialoug.stopLoading();
+        });
+    };
+
+    pub.footer = function () {
+        $('footer .footer-menu-4 form').on('submit', function (event) {
+            event.preventDefault();
+
+            var $form = $(this);
+            $('input[type="email"]', $form).removeClass('error');
+
+            $.post(this.action, $form.serialize(), function (response) {
+                var type = (response.status ? 'info' : 'error');
+                dialoug.notice(response.message, type, 3000, $form);
+
+                if (response.status) {
+                    $('input[type="text"], input[type="email"]', $form).val('');
+                } else {
+                    $('input[type="email"]', $form).addClass('error');
+                }
+            }, 'json');
+        });
+    };
+
+    pub.allover = function () {
+        // handling front page newsletter modal.
+        if ($('#newsletterModal').length) {
+            var $modal   = $('#newsletterModal');
+            var $trigger = $('#openNewsletterModal');
+
+            if ((!$.cookie('newsletter_prompt_off')) &&
+                (false === $('body').hasClass('is-mobile'))
+            ) {
+                $modal.show();
+            } else {
+                $trigger.fadeIn();
+            }
+
+            $('a.close-button', $modal).on('click', function (event) {
+                event.preventDefault();
+                $modal.hide();
+                $trigger.fadeIn();
+                $.cookie('newsletter_prompt_off', 1, {expires: 3650});
+            });
+
+            $('a', $trigger).on('click', function (e) {
+                e.preventDefault();
+                $trigger.hide();
+                $modal.fadeIn();
+            });
         }
-      }, 'json');
-    });
-  };
 
-  pub.allover = function() {
+        $('.js-newsletter-form .button').on('click', function (event) {
+            event.preventDefault();
+            var $this = $(this);
+            var $form = $this.closest('form');
+            var a     = $this.data('action');
+            var u     = $form.attr('action').replace(/[un]?subscribe/, a);
 
-//    // handeling .dk frontpage newsletter modal.
-//    if ($('#newsletterModal').length) {
-//        if (!$.cookie('newsletter_prompt_off') && (false === $('body').hasClass('is-mobile'))) {
-//            $('#newsletterModal').show();
-//        } else {
-//            $('#openNewsletterModal').fadeIn();
-//        }
-//        $('#newsletterModal a.close-button').on('click', function(event) {
-//            event.preventDefault();
-//            $('#newsletterModal').hide();
-//            $('#openNewsletterModal').fadeIn();
-//            $.cookie('newsletter_prompt_off', 1, {expires : 3650});
-//        });
-//        $('#openNewsletterModal a').on('click', function(e){
-//          e.preventDefault();
-//          $('#openNewsletterModal').hide();
-//          $('#newsletterModal').fadeIn();
-//        });
-//    }
+            $.post(u, $form.serialize(), function (response) {
+                var type = (response.status ? 'info' : 'error');
+                dialoug.notice(response.message, type, 3000, $form);
 
-    $('.js-newsletter-form .button').on('click', function(event) {
-      event.preventDefault();
-      var $this = $(this);
-      var $form = $this.closest('form');
-      var a = $this.data('action');
-      var u = $form.attr('action').replace(/[un]?subscribe/, a);
+                if (response.status) {
+                    $('input[type="text"], input[type="email"]', $form).val('');
+                    $('input[type="text"], input[type="email"]', $form).removeClass('error');
+                    $form.addClass('newsletter-subscribe-complete');
+                } else {
+                    $('input[type="email"]', $form).addClass('error');
+                }
+            }, 'json');
+        });
+    };
 
-      $.post(u, $form.serialize(), function(response) {
-        var type = (response.status? 'info' : 'error');
-        dialoug.notice(response.message, type, 3000, $form);
-
-        if (response.status) {
-          $('input[type="text"], input[type="email"]', $form).val('');
-          $('input[type="text"], input[type="email"]', $form).removeClass('error');
-          $form.addClass('newsletter-subscribe-complete');
-        } else {
-          $('input[type="email"]', $form).addClass('error');
-        }
-      }, 'json');
-    });
-  };
-
-  return pub;
+    return pub;
 })(jQuery);
 
 newsletter.footer();
