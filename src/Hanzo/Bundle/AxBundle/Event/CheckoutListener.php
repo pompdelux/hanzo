763890c08fc32f0446f5cc35beb254164cb84493
ulrik nielsen <ulrik@o3.dk>
2014-09-03 08:48:33 +0200

    Addresses #482 and demands full ax sync (and logging) before sending confirmation email

--- src/Hanzo/Bundle/AxBundle/Event/CheckoutListener.php
+++ src/Hanzo/Bundle/AxBundle/Event/CheckoutListener.php
@@ -23,21 +23,22 @@ class CheckoutListener
     /**
      * Sync order to AX
      *
-     * If this failes completly, propagation is halted.
+     * If this fails completely, propagation is halted.
      *
-     * @param  FilterOrderEvent $event [description]
-     * @return [type]                  [description]
+     * @param  FilterOrderEvent $event
      */
     public function onPaymentCollected(FilterOrderEvent $event)
     {
-        $order = $event->getOrder();
-        $this->ax->sendOrder($order, false, null, $event->getInEdit());
+        $order  = $event->getOrder();
+        $logged = false;
 
-        $logged = OrdersSyncLogQuery::create()
-            ->select('State')
-            ->filterByOrdersId($order->getId())
-            ->findOne(Propel::getConnection(null, Propel::CONNECTION_WRITE))
-        ;
+        if ($this->ax->sendOrder($order, false, null, $event->getInEdit())) {
+            $logged = OrdersSyncLogQuery::create()
+                ->select('State')
+                ->filterByOrdersId($order->getId())
+                ->findOne(Propel::getConnection(null, Propel::CONNECTION_WRITE))
+            ;
+        }
 
         if (!$logged) {
             $this->logger->addError('Order #'.$order->getId().' was not send to AX and error not written to db!');
