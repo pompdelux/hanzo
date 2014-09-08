d69867283ddf47603671ad4b683106c401e2ff0d
ulrik nielsen <ulrik@o3.dk>
2014-09-08 14:02:51 +0200

    new table added to keep track of orders in queue

---
+++ src/Hanzo/Bundle/AxBundle/Actions/Out/PheanstalkQueue.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the hanzo package.
+ *
+ * (c) Ulrik Nielsen <un@bellcom.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hanzo\Bundle\AxBundle\Actions\Out;
+
+
+class PheanstalkQueue {
+
+}
