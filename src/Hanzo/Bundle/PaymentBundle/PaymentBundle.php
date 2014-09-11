90d745db3cf4e677d131a3a72f291c0ce9efc1f6
ulrik nielsen <ulrik@o3.dk>
2014-09-11 14:52:27 +0200

    added compilerpass to find payment methods

--- src/Hanzo/Bundle/PaymentBundle/PaymentBundle.php
+++ src/Hanzo/Bundle/PaymentBundle/PaymentBundle.php
@@ -2,8 +2,18 @@
 
 namespace Hanzo\Bundle\PaymentBundle;
 
+use Hanzo\Bundle\PaymentBundle\DependencyInjection\Compiler\AggregatedTaggedServicesPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PaymentBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new AggregatedTaggedServicesPass());
+    }
 }
