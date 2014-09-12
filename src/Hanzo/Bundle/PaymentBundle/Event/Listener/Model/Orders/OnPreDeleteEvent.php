0e9a26e6da53de4cba60e3746474631e7eebb06a
ulrik nielsen <ulrik@o3.dk>
2014-09-12 08:09:38 +0200

    stuff moved to events

---
+++ src/Hanzo/Bundle/PaymentBundle/Event/Listener/Model/Orders/OnPreDeleteEvent.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is part of the hanzo package.
+ *
+ * (c) Ulrik Nielsen <un@bellcom.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hanzo\Bundle\PaymentBundle\Event\Listener\Model\Orders;
+
+use Glorpen\Propel\PropelBundle\Events\ModelEvent;
+use Hanzo\Bundle\PaymentBundle\PaymentActionsProxy;
+use Hanzo\Model\Orders;
+
+/**
+ * Class OnPreDeleteEvent
+ * @package Hanzo\Bundle\PaymentBundle
+ */
+class OnPreDeleteEvent
+{
+    private $actionsProxy;
+
+    public function __construct(PaymentActionsProxy $actionsProxy)
+    {
+        $this->actionsProxy = $actionsProxy;
+    }
+
+    public function handle(ModelEvent $event)
+    {
+        $order = $event->getModel();
+
+        if ((!$order instanceof Orders) ||
+            ($order->getState() < Orders::STATE_PAYMENT_OK)
+        ) {
+            return;
+        }
+
+        try {
+            $this->actionsProxy->cancelPayment($order);
+        } catch (\Exception $e) {
+            if (false === $order->getIgnoreDeleteConstraints()) {
+                throw $e;
+            }
+        }
+    }
+}
