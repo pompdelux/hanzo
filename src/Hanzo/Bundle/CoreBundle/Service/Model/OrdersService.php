af274e5f93dbf9e874a3bfa31630a6d6b28da7d7
ulrik nielsen <ulrik@o3.dk>
2014-09-15 11:14:29 +0200

    handeling order deletes

--- src/Hanzo/Bundle/CoreBundle/Service/Model/OrdersService.php
+++ src/Hanzo/Bundle/CoreBundle/Service/Model/OrdersService.php
@@ -10,7 +10,67 @@
 
 namespace Hanzo\Bundle\CoreBundle\Service\Model;
 
+use Hanzo\Bundle\AxBundle\Actions\Out\PheanstalkQueue;
+use Hanzo\Bundle\AxBundle\Logger;
+use Hanzo\Bundle\CoreBundle\Core;
+use Hanzo\Model\Orders;
+use Hanzo\Model\OrdersSyncLogQuery;
 
-class OrdersService {
+class OrdersService
+{
+    /**
+     * @var Core
+     */
+    private $hanzoCore;
 
+    /**
+     * @var PheanstalkQueue
+     */
+    private $pheanstalkQueue;
+
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * @param Core                $core
+     * @param PheanstalkQueue     $pheanstalkQueue
+     * @param Logger $logger
+     */
+    public function __construct(Core $core, PheanstalkQueue $pheanstalkQueue, Logger $logger)
+    {
+        $this->hanzoCore           = $core;
+        $this->pheanstalkQueue     = $pheanstalkQueue;
+        $this->logger              = $logger;
+    }
+
+    /**
+     * This triggers a series of events:
+     *   1. send delete command to ax (but only if it's already send to ax)
+     *   2. send delete command to payment provider
+     *   3. backup full order
+     *   4. delete order from db
+     *
+     * @param  Orders $order
+     * @return int
+     * @throws \Hanzo\Bundle\AxBundle\Actions\Out\OrderAlreadyInQueueException
+     */
+    public function deleteOrder(Orders $order)
+    {
+        $syncCheck = OrdersSyncLogQuery::create()
+            ->filterByOrdersId($order->getId())
+            ->filterByState('ok')
+            ->count($order->getDBConnection());
+
+        if ($syncCheck) {
+            $this->pheanstalkQueue->appendDeleteOrder($order);
+        }
+
+        try {
+            $order->delete($order->getDBConnection());
+        } catch (\Exception $e) {
+            $this->logger->write($order->getId(), 'failed', [], $e->getMessage());
+        }
+    }
 }
