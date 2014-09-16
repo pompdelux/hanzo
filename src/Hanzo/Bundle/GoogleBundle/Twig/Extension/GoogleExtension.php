141cd574139b7e393faee082664ba1ffdc0b9feb
ulrik nielsen <ulrik@o3.dk>
2014-09-16 11:30:27 +0200

    missing displayfeatures added

--- src/Hanzo/Bundle/GoogleBundle/Twig/Extension/GoogleExtension.php
+++ src/Hanzo/Bundle/GoogleBundle/Twig/Extension/GoogleExtension.php
@@ -111,6 +111,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', '{$this->analytics_code}', 'auto');
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
 
 {$ecommerce}
