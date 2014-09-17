9862ca9bd0c6b1905a28c4c87db44eca480d2f1e
ulrik nielsen <ulrik@o3.dk>
2014-09-17 12:01:53 +0200

    reworked dibs api to new queue method

--- src/Hanzo/Bundle/PaymentBundle/Controller/DibsController.php
+++ src/Hanzo/Bundle/PaymentBundle/Controller/DibsController.php
@@ -2,31 +2,27 @@
 
 namespace Hanzo\Bundle\PaymentBundle\Controller;
 
-use Propel;
-use Exception;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-
-use Hanzo\Core\Hanzo;
+use Hanzo\Core\CoreController;
+use Hanzo\Core\Tools;
+use Hanzo\Bundle\CheckoutBundle\Event\FilterOrderEvent;
+use Hanzo\Bundle\PaymentBundle\Methods\Dibs\DibsApi;
 use Hanzo\Model\Orders;
 use Hanzo\Model\OrdersPeer;
-use Hanzo\Model\AddressesPeer;
-use Hanzo\Core\Tools;
-use Hanzo\Core\CoreController;
-use Hanzo\Bundle\PaymentBundle\Methods\Dibs\DibsApi;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-use Hanzo\Bundle\CheckoutBundle\Event\FilterOrderEvent;
-
+/**
+ * Class DibsController
+ * @package Hanzo\Bundle\PaymentBundle
+ */
 class DibsController extends CoreController
 {
     /**
-     * callbackAction
+     * Handles callbacks from DIBS payment service in the payment flow
      *
-     * @return void
-     * @author Henrik Farre <hf@bellcom.dk>
-     **/
+     * @param  Request $request
+     * @return Response
+     */
     public function callbackAction(Request $request)
     {
         $api = $this->get('payment.dibsapi');
@@ -39,17 +35,17 @@ class DibsController extends CoreController
         }
 
         if (false === $payment_gateway_id) {
-            Tools::log('Dibs callback did not supply a valid payment gateway id');
-            Tools::log($_POST);
-            return new Response('Failed', 500, array('Content-Type' => 'text/plain'));
+            Tools::log('Dibs callback did not supply a valid payment gateway id POST: '.print_r($_POST, 1).' L: '.$request->getLocale());
+
+            return new Response('Failed', 500, ['Content-Type' => 'text/plain']);
         }
 
         $order = OrdersPeer::retriveByPaymentGatewayId($payment_gateway_id);
 
         if (!($order instanceof Orders)) {
-            Tools::log('No order matched payment gateway id: "'. $payment_gateway_id .'"');
-            Tools::log($_POST);
-            return new Response('Failed', 500, array('Content-Type' => 'text/plain'));
+            Tools::log('No order matched payment gateway id: "'. $payment_gateway_id .'" POST: '.print_r($_POST, 1).' L: '.$request->getLocale());
+
+            return new Response('Failed', 500, ['Content-Type' => 'text/plain']);
         }
 
         try {
@@ -57,33 +53,31 @@ class DibsController extends CoreController
             $api->updateOrderSuccess($request, $order);
 
             $this->get('event_dispatcher')->dispatch('order.payment.collected', new FilterOrderEvent($order));
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             Tools::log($e->getMessage());
             $api->updateOrderFailed($request, $order);
         }
 
-        return new Response('Ok', 200, array('Content-Type' => 'text/plain'));
+        return new Response('Ok', 200, ['Content-Type' => 'text/plain']);
     }
 
 
     /**
      * blockAction
      *
-     * @return void
-     * @author Henrik Farre <hf@bellcom.dk>
-     **/
+     * @return Response
+     */
     public function blockAction()
     {
-        $api = $this->get('payment.dibsapi');
-        $redis = $this->get('redis.permanent');
-
+Tools::log('YEAH - is used ..., delete log if seen.! <un>');
+        $api         = $this->get('payment.dibsapi');
+        $redis       = $this->get('redis.permanent');
         $dibs_status = $redis->hget('service.status', 'dibs');
-
-        $isJson = ('json' === $this->getFormat() ) ? true : false;
+        $isJson      = ('json' === $this->getFormat()) ? true : false;
 
         if (!$api->isActive() || ('DOWN' == $dibs_status)) {
             if ($isJson) {
-                return $this->json_response( array('status' => false) );
+                return $this->json_response(['status' => false]);
             } else {
 
                 $html = '';
@@ -91,61 +85,36 @@ class DibsController extends CoreController
                     $html = '<div class="down">'.$this->get('translator')->trans('dibs.down.message', [], 'checkout').'</div>';
                 }
 
-                return new Response( $html, 200, array('Content-Type' => 'text/html'));
+                return new Response($html, 200, ['Content-Type' => 'text/html']);
             }
         }
 
-        $order = OrdersPeer::getCurrent();
+        $order    = OrdersPeer::getCurrent();
         $settings = $api->buildFormFields($order);
-        $cardtypes = $api->getEnabledPaytypes();
 
         if ($isJson) {
-            return $this->json_response( array('status' => true, 'fields' => $settings) );
+            return $this->json_response(['status' => true, 'fields' => $settings]);
         } else {
-            return $this->render('PaymentBundle:Dibs:block.html.twig',array( 'cardtypes' => $cardtypes, 'form_fields' => $settings ));
+            return $this->render('PaymentBundle:Dibs:block.html.twig', ['cardtypes' => [], 'form_fields' => $settings]);
         }
     }
 
-    /**
-     * stateCheckAction
-     *
-     * Checks the current state of the order, this should allow the callback from dibs to be completed
-     *
-     * @return array
-     * @author Henrik Farre <hf@bellcom.dk>
-     **/
-    public function stateCheckAction()
-    {
-        $order   = OrdersPeer::getCurrent();
-        $goto    = false;
-        $session = $this->get('session');
-
-        if ($session->has('last_successful_order_id')) {
-            $goto = true;
-        }
-
-        return $this->json_response( array('state' => $order->getState(), 'redirect_to_basket' => $goto ) );
-    }
 
     /**
-     * processAction
+     * Used to see if our migration to the new flow works.
      *
-     * This shows the customer a page that checks the state of the ordre until it is correct (<= payment ok ) or fails
-     *
-     * @return object Response
-     * @author Henrik Farre <hf@bellcom.dk>
-     **/
-    public function processAction($order_id)
+     * @param  Request  $request
+     * @param  int      $order_id
+     * @return Response
+     */
+    public function processAction(Request $request, $order_id)
     {
-        $order = OrdersPeer::retriveByPaymentGatewayId( $order_id );
+        $order = OrdersPeer::retriveByPaymentGatewayId($order_id);
 
         if (!empty($order) && ($order->getId() !== $this->get('session')->get('order_id'))) {
-            error_log(__LINE__.':'.__FILE__.' Order id mismatch, in url: '.$order_id. ' in session: '. $this->getRequest()->getSession()->get('order_id') ); // hf@bellcom.dk debugging
+            Tools::log('Order id mismatch, in url: '.$order_id. ' in session: '. $request->getSession()->get('order_id').' L: '.$request->getLocale());
         }
 
-        return $this->render('PaymentBundle:Default:process.html.twig', [
-            'hide_mini_basket' => true,
-            'skip_my_account'  => true,
-        ]);
+        return $this->redirect($this->generateUrl('_checkout_success'));
     }
 }
