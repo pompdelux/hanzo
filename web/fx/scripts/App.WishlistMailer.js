e9c19ef8e30eb3509c08b38065876b21bfbb0fc0
ulrik nielsen <ulrik@o3.dk>
2014-11-04 17:51:42 +0100

    added wishlist listener to js build

--- web/fx/scripts/App.WishlistMailer.js
+++ web/fx/scripts/App.WishlistMailer.js
@@ -7,12 +7,12 @@ App.register('WishlistMailer', function() {
     var $_element;
     var $_trigger;
     var $_target;
+    var $_dialoug;
 
     publicMethods.init = function($element) {
         $_element = $element;
-        $_form    = $('form', $element);
-        $_target  = $('.message', $element);
-        $_trigger = $('a', $element);
+        $_trigger = $('a.js-email-trigger', $element);
+        $_dialoug = $('#js-send-wishlist');
 
         setupListeners();
     };
@@ -27,27 +27,37 @@ App.register('WishlistMailer', function() {
                 close: Translator.trans('close'),
                 overlayClose: true,
                 closeButton: true,
-                html: $_element.html()
-            });
-        });
-
-        $_form.on('submit', function(event) {
-            $_target.removeClass('msg error');
-            var xhr = $.post($_form.attr('action'), $_form.serialize());
-
-            xhr.done(function(response) {
-                if (response.status) {
-                    $_target.html(response.message);
-                    $_form.slideUp();
-                    $_target.slideDown();
-                } else {
-                    $_target.addClass('msg error');
-                    $_target.html(response.message);
-                }
+                html: $_dialoug.html()
             });
 
-            xhr.fail(function() {
-                console.log(arguments);
+            // we need to rescan the content to get the right elements
+            // they are injected into the colorbox element, and their initial location is therefor invalid
+            var $cb  = $('#cboxLoadedContent');
+            $_form   =  $('form', $cb);
+            $_target = $('.message', $cb);
+
+            $_form.on('submit', function(event) {
+                event.preventDefault();
+
+                $_target.addClass('off').removeClass('msg error');
+                var xhr = $.post($_form.attr('action'), $_form.serialize());
+
+                xhr.done(function(response) {
+                    if (response.status) {
+                        $_target.html(response.message);
+                        $_form.addClass('off');
+                        $_target.removeClass('off');
+                    } else {
+                        $_target.html(response.message);
+                        $_target.removeClass('off').addClass('msg error');
+                    }
+
+                    $.colorbox.resize();
+                });
+
+                xhr.fail(function() {
+                    console.log(arguments);
+                });
             });
         });
     };
