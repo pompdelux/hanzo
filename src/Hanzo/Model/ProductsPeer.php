74a2a35583cae5f67f1b29efca65bd8d15eb6d16
ulrik nielsen <ulrik@o3.dk>
2014-11-04 17:54:05 +0100

    ocd

--- src/Hanzo/Model/ProductsPeer.php
+++ src/Hanzo/Model/ProductsPeer.php
@@ -16,29 +16,29 @@ class ProductsPeer extends BaseProductsPeer
     /**
      * @param Request $request
      *
-     * @return mixed
+     * @return null|Products
      */
     public static function findFromRequest(Request $request)
     {
-        static $product_cache = array();
+        static $productCache = [];
 
         $key = self::mergeRequestParams();
 
-        if (isset($product_cache[$key])) {
-            return $product_cache[$key];
+        if (isset($productCache[$key])) {
+            return $productCache[$key];
         }
 
-        $product_cache[$key] = NULL;
-        $product_id = $request->request->get('product_id');
+        $productCache[$key] = null;
+        $productId = $request->request->get('product_id');
 
-        if ($product_id) {
-            $product_cache[$key] = ProductsQuery::findOneById($product_id);
+        if ($productId) {
+            $productCache[$key] = ProductsQuery::findOneById($productId);
         } else {
             $master = $request->request->get('master');
             $size   = $request->request->get('size');
             $color  = $request->request->get('color');
 
-            $product_cache[$key] = ProductsQuery::create()
+            $productCache[$key] = ProductsQuery::create()
                 ->filterByMaster($master)
                 ->filterBySize($size)
                 ->filterByColor($color)
@@ -46,11 +46,10 @@ class ProductsPeer extends BaseProductsPeer
                 ->useProductsDomainsPricesQuery()
                     ->filterByDomainsId(Hanzo::getInstance()->get('core.domain_id'))
                 ->endUse()
-                ->findOne()
-            ;
+                ->findOne();
         }
 
-        return $product_cache[$key];
+        return $productCache[$key];
     }
 
     /**
@@ -59,7 +58,7 @@ class ProductsPeer extends BaseProductsPeer
     protected static function mergeRequestParams()
     {
         $request = array_merge($_GET, $_POST);
-        $keys = array('master', 'size', 'color', 'product_id');
+        $keys = ['master', 'size', 'color', 'product_id'];
         foreach ($request as $key => $v) {
             if (!in_array($key, $keys)) {
                 unset($request[$key]);
