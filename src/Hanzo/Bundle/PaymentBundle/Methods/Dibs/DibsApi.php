ee14a5658331d41b96fc5607a707979750bab154
ulrik nielsen <ulrik@o3.dk>
2014-11-24 09:53:58 +0100

    added deadorder handeling

--- src/Hanzo/Bundle/PaymentBundle/Methods/Dibs/DibsApi.php
+++ src/Hanzo/Bundle/PaymentBundle/Methods/Dibs/DibsApi.php
@@ -3,10 +3,13 @@
 namespace Hanzo\Bundle\PaymentBundle\Methods\Dibs;
 
 use Hanzo\Model\Orders;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class DibsApi
+ *
  * @package Hanzo\Bundle\PaymentBundle\Methods\Dibs
  *
  * @method array       buildFormFields(Orders $order)
@@ -20,6 +23,20 @@ use Symfony\Component\HttpFoundation\Request;
 class DibsApi
 {
     /**
+     * Country code to currency map.
+     * DIBS support the currencies listed here: http://tech.dibspayment.com/toolbox/currency_codes
+     *
+     * @var array
+     */
+    protected $currencyMap = [
+        58  => 'DKK',
+        161 => 'NOK',
+        207 => 'SEK',
+        208 => 'CHF',
+    ];
+
+
+    /**
      * @var array
      */
     protected $settings = [];
@@ -28,6 +45,11 @@ class DibsApi
      * @var Router
      */
     protected $router;
+
+    /**
+     * @var PaymentFixer
+     */
+    protected $fixer;
 
     /**
      * Holds the active service type object
@@ -42,6 +64,8 @@ class DibsApi
     public function __construct($parameters, array $settings)
     {
         $this->router = $parameters[0];
+        $this->logger = $parameters[1];
+        $this->fixer  = $parameters[2];
 
         $this->settings = $settings;
         $this->settings['active'] = (isset($this->settings['method_enabled']) && $this->settings['method_enabled'] ? true : false);
@@ -77,5 +101,34 @@ class DibsApi
     public function __call($method, array $arguments = [])
     {
         return call_user_func_array([$this->service, $method], $arguments);
+    }
+
+    /**
+     * Forwards the request to handleStaleEdits() these cleanup
+     * tasks are the same for both stale and dead dibs orders
+     *
+     * @param Orders               $order
+     * @param OutputInterface|null $outputInterface
+     */
+    public function handleAbandoned(Orders $order, $outputInterface = null)
+    {
+        $this->handleStaleEdits($order, $outputInterface);
+    }
+
+    /**
+     * @param Orders               $order
+     * @param OutputInterface|null $outputInterface
+     *
+     * @return mixed
+     */
+    public function handleStaleEdits(Orders $order, $outputInterface = null)
+    {
+        // only cleanup orders with state less than "payment ok"
+        if ($order->getState() > Orders::STATE_PAYMENT_OK) {
+            return;
+        }
+
+        $this->fixer->setOutputInterface($outputInterface);
+        $this->fixer->resolve($order);
     }
 }
