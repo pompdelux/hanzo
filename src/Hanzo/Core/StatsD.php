00128c810e66c11ad9e8eaa65b30fbd285c33d4c
ulrik nielsen <ulrik@o3.dk>
2014-12-15 13:28:03 +0100

    added prefx to all metrics and also added route name to the buildtime metric key

--- src/Hanzo/Core/StatsD.php
+++ src/Hanzo/Core/StatsD.php
@@ -10,6 +10,9 @@
 
 namespace Hanzo\Core;
 
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
 /**
  * Class StatsD
  *
@@ -17,82 +20,153 @@ namespace Hanzo\Core;
  */
 class StatsD
 {
+    /**
+     * StatsD parameters
+     *
+     * @var array
+     */
     private $parameters = [
-        'enabled' => false,
         'host'    => '127.0.0.1',
         'port'    => 8125
     ];
 
+    /**
+     * Data to send to StatsD
+     *
+     * @var array
+     */
     private $data = [];
 
     /**
+     * StatsD key prefix
+     *
+     * @var string
+     */
+    private $prefix;
+
+    /**
+     * Name of the current route - if the request is an http(s) request
+     *
+     * @var string
+     */
+    private $routeName;
+
+    /**
+     * Whether or not the service is enabled
+     *
+     * @var bool
+     */
+    private $enabled = false;
+
+    /**
+     * Constructor
+     *
      * @param string $host
      * @param int    $port
+     * @param string $env
      */
-    public function __construct($host, $port = 8125)
+    public function __construct($host, $port = 8125, $env = '')
     {
         if ($host) {
-            $this->parameters['enabled'] = true;
+            $this->enabled = true;
             $this->parameters['host'] = $host;
             $this->parameters['port'] = $port;
+
+            list($env, ) = explode('_', $env, 2);
+            $this->prefix = $env.'.';
         }
     }
 
     /**
+     * Add timing log, note the $time variable is in milliseconds
+     *
      * @param string $variable
      * @param float  $time
      */
     public function timing($variable, $time)
     {
-        $this->data[] = "$variable:$time|ms";
+        $this->data[] = "{$this->prefix}{$variable}:{$time}|ms";
     }
 
     /**
+     * Add arbitrary gauge values
+     *
      * @param string $variable
      * @param float  $value
      */
     public function gauge($variable, $value)
     {
-        $this->data[] = "$variable:$value|g";
+        $this->data[] = "{$this->prefix}{$variable}:{$value}|g";
     }
 
     /**
+     *
      * @param string $variable
      * @param float  $value
      */
-    public function measure($variable, $value)
+    public function count($variable, $value)
     {
-        $this->data[] = "$variable:$value|c";
+        $this->data[] = "{$this->prefix}{$variable}:{$value}|c";
     }
 
     /**
+     * Increment a counter by one
+     *
      * @param string $variable
      */
     public function increment($variable)
     {
-        $this->data[] = "$variable:1|c";
+        $this->data[] = "{$this->prefix}{$variable}:1|c";
     }
 
     /**
+     * Decrement a counter by one
+     *
      * @param string $variable
      */
     public function decrement($variable)
     {
-        $this->data[] = "$variable:-1|c";
+        $this->data[] = "{$this->prefix}{$variable}:-1|c";
     }
 
+
     /**
-     * Flush the statsd cache to the statsd server.
+     * Used as a kernel.request event listener, and allows us to track buildtimes for named routes.
+     *
+     * @param GetResponseEvent $event
      */
-    public function flush()
+    public function onCoreHTTPRequest(GetResponseEvent $event)
     {
-        if (false === $this->parameters['enabled']) {
+        if ((false === $this->enabled)) {
             return;
         }
 
-        if (isset($_SERVER['REQUEST_TIME_FLOAT'])) {
+        $request = $event->getRequest();
+        if ($event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
+            return;
+        }
+
+        $route = $request->get('_route');
+        if ($route && !in_array($route, ['_wdt', 'is_authenicated', 'bazinga_jstranslation_js'])) {
+            $this->routeName = $route;
+        }
+    }
+
+
+    /**
+     * Flush the statsd cache to the statsd server.
+     *
+     * If the request is a http request, we add the buildtime for the request to the outgoing payload.
+     */
+    public function flush()
+    {
+        if (false === $this->enabled) {
+            return;
+        }
+
+        if (isset($_SERVER['REQUEST_TIME_FLOAT']) && $this->routeName) {
             // register the time (in milliseconds) it took to process the request.
-            $this->timing('buildtime', number_format(((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 100), 3, '.', ''));
+            $this->timing('buildtime.'.$this->routeName, number_format(((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 100), 3, '.', ''));
         }
 
         if (empty($this->data)) {
