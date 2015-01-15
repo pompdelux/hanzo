de3f9d2375fc02d436ba6311693fd6edc9883cc1
Henrik Farre <henrik@bellcom.dk>
2015-01-15 12:59:34 +0100

    doc + dumpXML option for debugging

--- src/Hanzo/Bundle/NewsletterBundle/Providers/MailPlatformRequest.php
+++ src/Hanzo/Bundle/NewsletterBundle/Providers/MailPlatformRequest.php
@@ -47,27 +47,35 @@ class MailPlatformRequest
     public $body;
 
     /**
+     * For debugging
+     *
+     * @var boolean
+     */
+    public $dumpXML = false;
+
+    /**
      * __construct
      *
      * @param string $username
      * @param string $token
+     * @param string $baseUrl
+     * @param string $query
      *
      * @return void
      * @author Henrik Farre <hf@bellcom.dk>
      */
-    public function __construct($username, $token, $query, $client)
+    public function __construct($username, $token, $baseUrl, $query)
     {
         $this->username = $username;
         $this->token    = $token;
         $this->query    = $query;
-        $this->client   = $client;
+        $this->client   = new \Guzzle\Http\Client($baseUrl);
     }
 
     /**
      * buildRequest
      *
-     *
-     * @return void
+     * @return mixed XML if ok request
      * @author Henrik Farre <hf@bellcom.dk>
      */
     protected function buildRequest()
@@ -82,11 +90,23 @@ class MailPlatformRequest
         return $xml->asXML();
     }
 
+    /**
+     * execute
+     *
+     * @return BaseResponse
+     * @author Henrik Farre <hf@bellcom.dk>
+     */
     public function execute()
     {
         $requestData = $this->buildRequest();
         $request = $this->client->post($this->query);
         $request->setBody($requestData);
+
+        if ($this->dumpXML === true)
+        {
+            error_log(__LINE__.':'.__FILE__.':> '.PHP_EOL.$requestData); // hf@bellcom.dk debugging
+        }
+
         $rawResponse = $request->send();
 
         $parser = new MailPlatformResponseParser($rawResponse, $this);
@@ -109,6 +129,7 @@ class MailPlatformRequest
          * So we loop over the field, e.g. 'customfields', and count how many sub elements ('item') it contains
          * For each of those sub elements a new xml child is added, and then looped over again
          *
+         * Fields of type Dropdown/date are currently not supported because they require a extra sub element of key, see http://mailmailmail.net/xmlguide/index.php?rt=Subscribers&rm=Update
          */
         $multipleValueFields = ['customfields' => ['item' => ['fieldid','value']]];
 
