1ba449bdc6890c634230a2ed226db9e032a055d6
Henrik Farre <henrik@bellcom.dk>
2015-01-22 11:27:27 +0100

    Use the configured code for the domain

--- src/Hanzo/Bundle/GoogleBundle/Twig/Extension/GoogleExtension.php
+++ src/Hanzo/Bundle/GoogleBundle/Twig/Extension/GoogleExtension.php
@@ -216,13 +216,13 @@ DOC;
 
         $html = <<<DOC
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NRF34M"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id={$this->google_tag_manager_id}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NRF34M');</script>
+})(window,document,'script','dataLayer',"{$this->google_tag_manager_id}");</script>
 <!-- End Google Tag Manager -->
 DOC;
 
