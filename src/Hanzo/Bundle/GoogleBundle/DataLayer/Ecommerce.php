de8af32e3b5681a3a38198cb69655d78a199b918
Henrik Farre <henrik@bellcom.dk>
2015-03-13 16:37:20 +0100

    Datalayer clases

---
+++ src/Hanzo/Bundle/GoogleBundle/DataLayer/Ecommerce.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Hanzo\Bundle\GoogleBundle\DataLayer;
+
+class Ecommerce extends AbstractDataLayer
+{
+    public function __construct($page_type, Array $context = [])
+    {
+        if ($page_type != 'checkout-success') {
+            return;
+        }
+
+        $order = $context['order'];
+
+        $purchase = [];
+        $purchase['actionField'] = [
+            'id'         =>  $order['id'],
+            'affiliation'=>  $order['store_name'],
+            'revenue'    =>  $order['total'],
+            'shipping'   =>  $order['shipping'],
+            'tax'        =>  $order['tax'],
+            'currency'   =>  $order['currency'],
+        ];
+
+        $purchase['products'] = [];
+        foreach ($order['lines'] as $line) {
+            $product = [
+                'id'       => $order['id'],
+                'name'     => $line['name'],
+                'sku'      => $line['sku'],
+                'category' => $line['variation'],
+                'price'    => $line['price'],
+                'quantity' => $line['quantity'],
+            ];
+
+            $purchase['products'][] = $product;
+        }
+
+        $this->data = ['ecommerce' => [ 'purchase' => $purchase ]];
+    }
+}
