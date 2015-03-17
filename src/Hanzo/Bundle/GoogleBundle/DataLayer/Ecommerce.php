d6401245bdfc5806b497f51c4270ed6507e91835
Henrik Farre <henrik@bellcom.dk>
2015-03-17 14:49:47 +0100

    Adapt to abstract class, check for order

--- src/Hanzo/Bundle/GoogleBundle/DataLayer/Ecommerce.php
+++ src/Hanzo/Bundle/GoogleBundle/DataLayer/Ecommerce.php
@@ -4,9 +4,13 @@ namespace Hanzo\Bundle\GoogleBundle\DataLayer;
 
 class Ecommerce extends AbstractDataLayer
 {
-    public function __construct($page_type, Array $context = [])
+    public function __construct($page_type = '', Array $context = [], Array $params = [])
     {
         if ($page_type != 'checkout-success') {
+            return;
+        }
+
+        if (!isset($context['order'])) {
             return;
         }
 
