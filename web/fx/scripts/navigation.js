9481e8748745ab0eb7a56e472179a647191befbb
Morten <mn@bellcom.dk>
2015-04-29 11:32:47 +0200

    Made a seperate navigation.js and removed navigation related from main.js

---
+++ web/fx/scripts/navigation.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+
+    //// menu handeling
+    //if (false === $('body').hasClass('is-mobile')) {
+    //    var $menu = $('nav.main-menu');
+    //
+    //    // set parent li's class to active for active elements.
+    //    $('nav.first.main-menu .active')
+    //        .parents('li')
+    //        .toggleClass('active inactive')
+    //    ;
+    //
+    //    var menu_width = 0;
+    //    $('> ul > li > ul > li.heading', $menu).each(function (index, element) {
+    //        var $element = $(element);
+    //        var tmp_width = $element.outerWidth();
+    //        if (menu_width < tmp_width) {
+    //            menu_width = tmp_width;
+    //        }
+    //
+    //        $element.addClass('floaded');
+    //    });
+    //    $('> ul > li > ul > li.heading', $menu).closest('ul').each(function (index, element) {
+    //        var $element = $(element);
+    //        var count = $('> li', $element).length;
+    //        $element.css('width', (menu_width * count));
+    //    });
+    //
+    //    // Close filters dropdown
+    //    $('nav.filters-dropdowns')
+    //        .find('.outer > li')
+    //        .find('.fa-angle-up')
+    //        .on('click', function() {
+    //
+    //            // Run through all containers
+    //            $('nav.filters-dropdowns ').find('ul.outer > li').each(function(key, value) {
+    //
+    //                // Remove open class
+    //                if($(this).hasClass('open')) {
+    //                    //$(this).removeClass();
+    //                    $(this).removeClass('open');
+    //                }
+    //
+    //                // Remove on class
+    //                if($(this).hasClass('on')) {
+    //                    //$(this).removeClass();
+    //                    $(this).removeClass('on');
+    //                }
+    //
+    //                // $(this).find('> ul').hide();
+    //            });
+    //        });
+    //}
+
+    // Main navigation
+    if(Modernizr.touch) {
+
+        // Configuration
+        var navigation = $('nav.category-menu'),
+            navigation_container = navigation.find('> ul.outer'),
+            toggle_links = navigation_container.find('> li > a');
+
+        // Click on visible link (toggle)
+        toggle_links.on('click', function(event) {
+
+            // Get configurations
+            var parent = $(this).parent(),
+                dropdown_menu = $(this).parent().find('> ul');
+
+            // Has dropdown menu
+            if(dropdown_menu.length > 0) {
+
+                // Toggle 'open' class
+                parent.toggleClass('open');
+
+                // Don't follow link
+                event.preventDefault();
+            }
+        });
+    }
+
+    //    navigation_link_container = navigation.find('ul.outer');
+    //
+    //// Link is clicked (first layer links)
+    //navigation_link_container.find('> li > a').on('click', function(event) {
+    //    var parent = $(this).parent(),
+    //        dropdown_menu = $(this).parent().find('> ul');
+    //
+    //    // Link has a submenu
+    //    if(dropdown_menu.length > 0) {
+    //
+    //        // Toggle 'open' class
+    //        parent.toggleClass('open');
+    //
+    //        // Don't follow links href
+    //        event.preventDefault();
+    //    }
+    //});
+
+    // Filters
+    if(Modernizr.touch) {
+
+        
+    }
+});
