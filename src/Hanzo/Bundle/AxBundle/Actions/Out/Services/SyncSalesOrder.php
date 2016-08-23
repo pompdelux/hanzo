98015f4ea82b30061bf1a1b94f4f03b0b563566f
Ulrik Nielsen <me@ulrik.co>
2016-08-23 13:15:00 +0200

    added invoice payment type

--- src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php
+++ src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php
@@ -589,7 +589,7 @@ class SyncSalesOrder extends BaseService
                 break;
 
             case 'invoicepayment':
-                $custPaymMode = 'faktura';
+                $custPaymMode = 'Bank';
                 break;
 
             case 'pensio':
