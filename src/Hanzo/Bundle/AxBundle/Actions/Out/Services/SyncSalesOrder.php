5d9f28795286e9fa9dcf5b04a7d1921fc78877d0
Ulrik Nielsen <me@ulrik.co>
2016-08-25 09:49:00 +0200

    changed stuff

--- src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php
+++ src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php
@@ -575,7 +575,7 @@ class SyncSalesOrder extends BaseService
                 break;
 
             case 'invoicepayment':
-                $custPaymMode = 'faktura';
+                $custPaymMode = 'Faktura';
                 break;
 
             case 'pensio':
