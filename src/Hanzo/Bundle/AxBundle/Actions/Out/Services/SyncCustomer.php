e32dbf706bece9ed4e6d05695ae07fa3ec63319a
Ulrik Nielsen <me@ulrik.co>
2016-08-29 13:51:25 +0200

    to not break test

--- src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncCustomer.php
+++ src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncCustomer.php
@@ -107,7 +107,7 @@ class SyncCustomer extends BaseService
                     'CustName'               => $this->address->getName($this->translator),
                     'Email'                  => $this->customer->getEmail(),
                     'Phone'                  => $this->customer->getPhone(),
-                    'MayContact'             => $this->customer->getMayBeContacted(),
+//                    'MayContact'             => $this->customer->getMayBeContacted(),
                 ]
             ],
             'endpointDomain' => $this->getEndPoint(),
