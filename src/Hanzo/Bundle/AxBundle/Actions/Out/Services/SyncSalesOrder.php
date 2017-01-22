e00f649cd515718501787ad094d3643a8818e0a3
Ulrik Nielsen <me@ulrik.co>
2017-01-22 10:24:33 +0100

    ajusted promo products for SS17

--- src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php
+++ src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php
@@ -305,9 +305,11 @@ class SyncSalesOrder extends BaseService
             /*
              * remove POMPBIGBAGSS15 #998, https://github.com/pompdelux/hanzo/blob/f6a8cf650c7aa1344f17979118a497786e6b23f7/src/Hanzo/Bundle/AxBundle/Actions/Out/Services/SyncSalesOrder.php#L297
              */
-            $bagPrice  = 0.00;
-            $salesQty  = 1; // AX does not handle the same line twice, so add 2 here, and in buildPromotions we check if it is set
-            $itemId    = 'FREEPOMPBIGBAGAW16';
+            $bagPrice = 0.00;
+            $keyPrice = 0.00;
+            $salesQty = 1; // AX does not handle the same line twice, so add 2 here, and in buildPromotions we check if it is set
+            $itemId   = 'FREEPOMPBIGBAGSS17';
+            $keyId    = 'BloraKeychainSS17';
 
             switch($domainKey) {
                 case 'AT':
@@ -316,16 +318,20 @@ class SyncSalesOrder extends BaseService
                 case 'FI':
                 case 'NL':
                     $bagPrice = '4.99';
+                    $keyPrice = '2.00';
                     break;
                 case 'CH':
                     $bagPrice = '5.90';
+                    $keyPrice = '2.50';
                     break;
                 case 'DK':
                     $bagPrice = '40.00';
+                    $keyPrice = '15.00';
                     break;
                 case 'NO':
                 case 'SE':
                     $bagPrice = '60.00';
+                    $keyPrice = '20.00';
                     break;
             }
 
@@ -334,12 +340,23 @@ class SyncSalesOrder extends BaseService
                 'SalesPrice'      => $bagPrice,
                 'LineDiscPercent' => 100,
                 'SalesQty'        => $salesQty,
-                'InventColorId'   => 'Navy',
+                'InventColorId'   => 'Off White',
                 'InventSizeId'    => 'One Size',
                 'SalesUnit'       => 'Stk.',
             ];
-
             $this->promotionsAdded[$itemId] = $salesQty;
+
+            // Key hanger - First added SS17
+            $this->data['salesOrder']['SalesTable']['SalesLine'][] = [
+                'ItemId'          => $keyId,
+                'SalesPrice'      => $keyPrice,
+                'LineDiscPercent' => 100,
+                'SalesQty'        => $salesQty,
+                'InventColorId'   => 'Rose',
+                'InventSizeId'    => 'One Size',
+                'SalesUnit'       => 'Stk.',
+            ];
+            $this->promotionsAdded[$keyId] = $salesQty;
         }
     }
 
@@ -358,16 +375,16 @@ class SyncSalesOrder extends BaseService
             (in_array($this->getAttribute('purchase', 'type'), ['other', 'gift', 'private', 'friend'], true))
         ) {
 
-            $fromDate = 20160825;
-            $toDate   = 20161106;
-            $itemId   = 'FREEPOMPBAGAW16';
+            $fromDate = 20170223;
+            $toDate   = 20170430;
+            $itemId   = 'FREEPOMPBAGSS17';
 
             // Also check if promotion has been added already
             if (!isset($this->promotionsAdded[$itemId]) && (($fromDate <= $date) && ($toDate >= $date)) ||
                 ($this->inEdit && ($toDate >= $this->order->getCreatedAt('Ymd')))
             ) {
-                $bagPrice  = 0.00;
-                $salesQty  = 1; // AX does not handle the same line twice, so add 2 here, and in buildPromotions we check if it is set
+                $bagPrice = 0.00;
+                $salesQty = 1; // AX does not handle the same line twice, so add 2 here, and in buildPromotions we check if it is set
 
                 switch($domainKey) {
                     case 'AT':
@@ -376,7 +393,7 @@ class SyncSalesOrder extends BaseService
                     case 'FI':
                     case 'NL':
                         $bagPrice = '1.99';
-                        break;
+                    break;
                     case 'CH':
                         $bagPrice = '1.90';
                         break;
@@ -385,7 +402,7 @@ class SyncSalesOrder extends BaseService
                         break;
                     case 'NO':
                     case 'SE':
-                        $bagPrice = '15.00';
+                        $bagPrice = '20.00';
                         break;
                 }
 
@@ -394,7 +411,7 @@ class SyncSalesOrder extends BaseService
                     'SalesPrice'      => $bagPrice,
                     'LineDiscPercent' => 100,
                     'SalesQty'        => $salesQty,
-                    'InventColorId'   => 'Navy',
+                    'InventColorId'   => 'Off White',
                     'InventSizeId'    => 'One Size',
                     'SalesUnit'       => 'Stk.',
                 ];
